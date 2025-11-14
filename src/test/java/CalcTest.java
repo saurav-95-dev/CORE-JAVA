2c86a76ae28193b01d29fb764f074aa03917d4ce
Saurabh <devsaurabh086@gmail.com>
2025-11-14 22:58:44 +0530

    CalcTest.java-Test/java/testFile

---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,12 @@
+package in.saurabhdev.calc;
+
+public class Calc {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
