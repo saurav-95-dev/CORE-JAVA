6801e7df1087380650cb9c4c876e49fd87126370
Saurabh <devsaurabh086@gmail.com>
2025-11-15 23:57:38 +0530

    @BeforeEach and @AfterEach--tests

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -1,27 +1,30 @@
 package in.saurabhdev.calc;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalcTest {
 
+    Calc calc;
+
+    @BeforeEach
+    void setup() {
+        calc = new Calc();
+        System.out.println("BeforeEach: New Calc object created");
+    }
+
+    @AfterEach
+    void teardown() {
+        System.out.println("AfterEach: Test finished\n");
+    }
+
     @Test
     void testAdd() {
-        Calc c = new Calc();
-        int result = c.add(10, 20);
-        assertEquals(30, result);
+        assertEquals(30, calc.add(10,20));
     }
 
     @Test
     void testDivide() {
-        Calc c = new Calc();
-        int result = c.divide(10, 2);
-        assertEquals(5, result);
-    }
-
-    @Test
-    void testDivideByZero() {
-        Calc c = new Calc();
-        assertThrows(ArithmeticException.class, () -> c.divide(10, 0));
+        assertEquals(5, calc.divide(10,2));
     }
 }
