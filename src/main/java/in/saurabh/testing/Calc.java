12c7ff3c59c075c5389abeb55a7f777217ec6b3e
Saurabh <devsaurabh086@gmail.com>
2025-11-16 22:17:36 +0530

    Test class created for Calc.java

---
+++ src/main/java/in/saurabh/testing/Calc.java
@@ -0,0 +1,7 @@
+package in.saurabh.testing;
+
+class Calc{
+    public int divide(int num1 , int num2){
+        return num1/num2;
+    }
+}
---
+++ src/test/java/in/saurabh/testing/CalcTest.java
@@ -0,0 +1,9 @@
+package in.saurabh.testing;
+
+import junit.framework.TestCase;
+
+public class CalcTest extends TestCase {
+
+    public void testDivide() {
+    }
+}
