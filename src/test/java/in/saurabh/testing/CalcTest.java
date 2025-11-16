a63068b38c2fbe7b203616b3ad87c04242168675
Saurabh <devsaurabh086@gmail.com>
2025-11-16 23:29:21 +0530

    Running tests on multiple/individual functions via JUNIT-Jupiter

--- src/test/java/in/saurabh/testing/CalcTest.java
+++ src/test/java/in/saurabh/testing/CalcTest.java
@@ -3,10 +3,15 @@ package in.saurabh.testing;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalcTest {
-
+    Calc calc = new Calc();
     @org.junit.jupiter.api.Test
-    void test() {
-        Calc calc = new Calc();
+    void testReverse1() {
+
         assertEquals("ved" , calc.reverseString("dev") );
+    }
+    @org.junit.jupiter.api.Test
+    void testReverse2() {
+
+        assertEquals("eman yM" , calc.reverseString("My name") );
     }
 }
