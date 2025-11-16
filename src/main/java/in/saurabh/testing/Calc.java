ece9a2964746bc2e030b23c9452711b27a2e9836
Saurabh <devsaurabh086@gmail.com>
2025-11-16 22:26:30 +0530

    First JUNIT-5 Test-case for divide-unit

--- src/main/java/in/saurabh/testing/Calc.java
+++ src/main/java/in/saurabh/testing/Calc.java
@@ -1,6 +1,7 @@
 package in.saurabh.testing;
 
 class Calc{
+    //this is a simple non-static method:
     public int divide(int num1 , int num2){
         return num1/num2;
     }
--- src/test/java/in/saurabh/testing/CalcTest.java
+++ src/test/java/in/saurabh/testing/CalcTest.java
@@ -5,5 +5,9 @@ import junit.framework.TestCase;
 public class CalcTest extends TestCase {
 
     public void testDivide() {
+        Calc calc = new Calc();
+        int actualResult = calc.divide(12,6);
+        int expectedResult = 2;
+        assertEquals(expectedResult, actualResult);
     }
 }
