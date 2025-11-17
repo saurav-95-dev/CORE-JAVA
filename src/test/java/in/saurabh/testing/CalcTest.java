d03581f161e0edfffe54bfae03c634744b5118e9
Saurabh <devsaurabh086@gmail.com>
2025-11-17 21:19:32 +0530

    Integrating maven-surefire-plugin in pom.xml to run tests

--- src/test/java/in/saurabh/testing/CalcTest.java
+++ src/test/java/in/saurabh/testing/CalcTest.java
@@ -3,6 +3,7 @@ package in.saurabh.testing;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalcTest {
+
     Calc calc = new Calc();
     @org.junit.jupiter.api.Test
     void testReverse1() {
