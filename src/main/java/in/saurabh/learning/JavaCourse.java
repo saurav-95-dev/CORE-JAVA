9e24ff62abc7513ce1b04e0f9d34bea65b0110f1
Saurabh <devsaurabh086@gmail.com>
2025-11-18 22:55:26 +0530

    Revoved all Course files

--- src/main/java/in/saurabh/learning/JavaCourse.java
+++
@@ -1,8 +0,0 @@
-package in.saurabh.learning;
-public class JavaCourse implements Course {
-    @Override
-    public boolean coursePurchase(){
-        System.out.println("Java course purchase");
-        return true;
-    }
-}
