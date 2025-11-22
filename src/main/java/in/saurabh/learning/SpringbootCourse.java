046fc4731f14018c9f82956009302ab18feb3b09
Saurabh <devsaurabh086@gmail.com>
2025-11-22 22:17:30 +0530

    Reverted all changes

--- src/main/java/in/saurabh/learning/SpringbootCourse.java
+++
@@ -1,9 +0,0 @@
-package in.saurabh.learning;
-
-public class SpringbootCourse implements Course {
-    @Override
-    public boolean coursePurchase() {
-        System.out.println("Springboot course purchase");
-        return true;
-    }
-}
--- src/main/java/in/saurabh/learning/junitCourse.java
+++
@@ -1,9 +0,0 @@
-package in.saurabh.learning;
-
-public class junitCourse implements Course{
-    @Override
-    public boolean coursePurchase(){
-        System.out.println();
-        return true;
-    }
-}
