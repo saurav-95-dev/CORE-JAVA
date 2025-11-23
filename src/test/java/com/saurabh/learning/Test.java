6819109daa89f4689327f6afa3c988360dcf9c35
Saurabh <devsaurabh086@gmail.com>
2025-11-23 20:44:55 +0530

    Re-structuring folder JUNIT-5 folder sructure and updating pom.xml

---
+++ src/test/java/com/saurabh/learning/Test.java
@@ -0,0 +1,10 @@
+package com.saurabh.learning;
+import static org.junit.Assert.assertEquals;
+
+class Test{
+    @org.junit.jupiter.api.Test
+    void test(){
+        assertEquals(6 , 6);
+
+    }
+}
