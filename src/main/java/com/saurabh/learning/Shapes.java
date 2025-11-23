9ae7abad7d67c4678b5ca148f50249468284b768
Saurabh <devsaurabh086@gmail.com>
2025-11-23 21:45:37 +0530

    Creating JUNIT-5 Test for Shapes.java-singleFile-assertEquals()

---
+++ src/main/java/com/saurabh/learning/Shapes.java
@@ -0,0 +1,13 @@
+package com.saurabh.learning ;
+
+public class Shapes{
+
+    public double squareArea(double length){
+        return length * length ;
+    }
+
+    public double circleArea(double radius){
+        return radius *radius ;
+    }
+
+}
--- src/test/java/com/saurabh/learning/Test.java
+++ src/test/java/com/saurabh/learning/Test.java
@@ -5,6 +5,14 @@ class Test{
     @org.junit.jupiter.api.Test
     void test(){
         assertEquals(6 , 6);
-
     }
+
+    @org.junit.jupiter.api.Test
+    void test2(){
+        Shapes s = new Shapes();
+        double actualResult = s.squareArea(12);
+        double expectedResult = 144.0;
+        assertEquals(expectedResult , actualResult , 0.0001);
+    }
+
 }
