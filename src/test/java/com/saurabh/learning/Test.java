8b5f942963b37097fd9fc9683c766fcafb66a705
Saurabh <devsaurabh086@gmail.com>
2025-11-24 22:08:10 +0530

    Passing error message via supplier-interface (a functional interface)

--- src/test/java/com/saurabh/learning/Test.java
+++ src/test/java/com/saurabh/learning/Test.java
@@ -2,7 +2,8 @@ package com.saurabh.learning;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-class Test{
+class Test {
+
     @org.junit.jupiter.api.Test
     void test(){
         assertEquals(6 , 6);
@@ -12,9 +13,18 @@ class Test{
     void test2(){
         Shapes s = new Shapes();
         double actualResult = s.squareArea(12);
-        double expectedResult = 134.0;
-        assertEquals(expectedResult , actualResult , 0.0001 , "Sorry buddy , your tests are failer");
+        double expectedResult = 144.0;
+        assertEquals(expectedResult, actualResult, 0.0001, "Sorry buddy , your tests are failer");
     }
 
+    @org.junit.jupiter.api.Test
+    void test2_viaSuplierInterface(){
+        Shapes s = new Shapes();
+        double actualResult = s.squareArea(12);
+        double expectedResult = 124.0;
 
+        // Using Supplier<String> for custom message
+        assertEquals(expectedResult, actualResult, 0.0001,
+                () -> "Sorry buddy , your tests are failer");
+    }
 }
