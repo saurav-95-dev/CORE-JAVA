100780f44d0e445d921cde85bbe3b6d3c5ec93ec
Saurabh <devsaurabh086@gmail.com>
2025-11-29 19:44:04 +0530

    Testing with JUNIT-4 Calc.java--test_divide()

---
+++ src/test/java/com/saurabh/learning/CalcTest.java
@@ -0,0 +1,15 @@
+package com.saurabh.learning;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalcTest {
+    @Test
+    public void test_divide() {
+        Calc calc = new Calc();
+        int actualResult = calc.divide(10, 2);
+        int expectedResult = 5;
+        assertEquals(expectedResult, actualResult);
+    }
+}
