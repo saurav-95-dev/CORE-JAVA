ca9cf2c7183358d8de57c1c492c37f2ff080fe17
Saurabh <devsaurabh086@gmail.com>
2025-12-01 13:46:51 +0530

    Running multiple tests of same method

--- src/test/java/com/saurabh/learning/ReverseStringTest.java
+++ src/test/java/com/saurabh/learning/ReverseStringTest.java
@@ -7,11 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ReverseStringTest{
 
     ReverseString rs = new ReverseString();
-    @org.junit.jupiter.api.Test
-    void reverseString() {
-        String actualResult = rs.reverseString("dev");
-        String expectedResult = "ved";
-        assertEquals(expectedResult, actualResult);
 
+    @org.junit.jupiter.api.Test
+    void reverseString_singleWord() {
+        String expectedResult = "ved";
+        assertEquals(expectedResult, rs.reverseString("dev"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void reverseString_multipleWords(){
+       String expectedResult = "ved hbaruas";
+       assertEquals(expectedResult, rs.reverseString("saurabh dev"));
     }
 }
