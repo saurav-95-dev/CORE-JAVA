631c1ba3e8eb05ec6accd6afa0503843b583b78b
Saurabh <devsaurabh086@gmail.com>
2025-12-02 17:42:17 +0530

    ShapesTest.java--Supplier-Interface

--- src/test/java/com/saurabh/learning/ShapesTest.java
+++ src/test/java/com/saurabh/learning/ShapesTest.java
@@ -8,12 +8,19 @@ class ShapesTest {
     @Test
     void squareArea() {
         Shapes shapes = new Shapes();
-        assertEquals(576 , shapes.squareArea(24));
+        assertEquals(576 , shapes.squareArea(24) , ()->" Square area is wrong");
     }
      @Test
     void circleArea(){
         Shapes shapes = new Shapes();
         assertEquals( 28.26 , shapes.circleArea(3) , 0.001);
     }
+    @Test
+    void circleArea_supplierInterface(){
+        Shapes shapes = new Shapes();
+        assertEquals( 28.26 , shapes.circleArea(3) , 0.001 , ()->" Circle area is wrong");
+    }
 
 }
+
+//Supplier interface would only evaluate failed string in-case if the test-case fails.
