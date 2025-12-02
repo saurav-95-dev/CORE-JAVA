3acfa13aa641c5df0ed445c3853634439fcedb12
Saurabh <devsaurabh086@gmail.com>
2025-12-02 17:14:22 +0530

    Running tests for multiple methods inside a class

---
+++ src/main/java/com/saurabh/learning/Shapes.java
@@ -0,0 +1,14 @@
+package com.saurabh.learning;
+
+public class Shapes{
+
+    public double squareArea(double length){
+        double result = length *length;
+        return result;
+    }
+
+    public double circleArea(double radius){
+        double circle = 3.14 * radius * radius;
+        return circle;
+    }
+}
---
+++ src/test/java/com/saurabh/learning/ShapesTest.java
@@ -0,0 +1,19 @@
+package com.saurabh.learning;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShapesTest {
+
+    @Test
+    void squareArea() {
+        Shapes shapes = new Shapes();
+        assertEquals(576 , shapes.squareArea(24));
+    }
+     @Test
+    void circleArea(){
+        Shapes shapes = new Shapes();
+        assertEquals( 28.26 , shapes.circleArea(3));
+    }
+
+}
