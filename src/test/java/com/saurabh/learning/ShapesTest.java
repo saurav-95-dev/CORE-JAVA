203f239dc703c82d5f3d80b05d6e28ce94dfd2bc
Saurabh <devsaurabh086@gmail.com>
2025-12-05 23:50:31 +0530

    Reverting tests

--- src/test/java/com/saurabh/learning/ShapesTest.java
+++ src/test/java/com/saurabh/learning/ShapesTest.java
@@ -5,16 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ShapesTest {
 
-    @Test
-    void squareArea() {
-        Shapes shapes = new Shapes();
-        assertEquals(576 , shapes.squareArea(24) , ()->" Square area is wrong");
-    }
-     @Test
-    void circleArea(){
-        Shapes shapes = new Shapes();
-        assertEquals( 28.26 , shapes.circleArea(3) , 0.001);
-    }
 
 
 }
