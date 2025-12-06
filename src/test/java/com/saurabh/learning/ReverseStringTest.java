27bf6a4fc7da15229c4939342dff60e9f7978343
Saurabh <devsaurabh086@gmail.com>
2025-12-06 23:29:31 +0530

    Reverting tests for Calc.java and ReverseString.java

--- src/test/java/com/saurabh/learning/ReverseStringTest.java
+++ src/test/java/com/saurabh/learning/ReverseStringTest.java
@@ -1,22 +1,22 @@
-package com.saurabh.learning;
-
-import org.junit.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-public class ReverseStringTest{
-
-    ReverseString rs = new ReverseString();
-
-    @org.junit.jupiter.api.Test
-    void reverseString_singleWord() {
-        String expectedResult = "ved";
-        assertEquals(expectedResult, rs.reverseString("dev"));
-    }
-
-    @org.junit.jupiter.api.Test
-    void reverseString_multipleWords(){
-       String expectedResult = "ved hbaruas";
-       assertEquals(expectedResult, rs.reverseString("saurabh dev"));
-    }
-}
+//package com.saurabh.learning;
+//
+//import org.junit.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//public class ReverseStringTest{
+//
+//    ReverseString rs = new ReverseString();
+//
+//    @org.junit.jupiter.api.Test
+//    void reverseString_singleWord() {
+//        String expectedResult = "ved";
+//        assertEquals(expectedResult, rs.reverseString("dev"));
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void reverseString_multipleWords(){
+//       String expectedResult = "ved hbaruas";
+//       assertEquals(expectedResult, rs.reverseString("saurabh dev"));
+//    }
+//}
