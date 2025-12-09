d13f3274b24373f10d1680ee54ba07bcbca6adb5
Saurabh <devsaurabh086@gmail.com>
2025-12-09 23:03:21 +0530

    Creating TestException.java

---
+++ src/test/java/com/saurabh/learning/TestException.java
@@ -0,0 +1,12 @@
+package com.saurabh.learning;
+
+import org.junit.jupiter.api.Test;
+
+public class TestException {
+    @Test
+    void testException(){
+        Exception ex = new Exception();
+
+
+    }
+}
