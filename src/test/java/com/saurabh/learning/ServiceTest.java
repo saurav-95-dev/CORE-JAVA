e5882d3bf316550bff98e0c1d393edf0b4a94b7c
Saurabh <devsaurabh086@gmail.com>
2025-12-14 23:34:57 +0530

    Service class root folder changed

--- src/test/java/com/saurabh/learning/ServiceTest.java
+++ src/test/java/com/saurabh/learning/ServiceTest.java
@@ -1,3 +1,5 @@
+package com.saurabh.learning;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
