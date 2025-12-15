9d8b8aa94ff6bd7eab8f9229bc6563e09aac5a5b
Saurabh <devsaurabh086@gmail.com>
2025-12-15 20:06:33 +0530

    Service.java reverted

--- src/main/java/com/dev/learning/Service.java
+++
@@ -1,9 +0,0 @@
-package com.dev.learning;
-public class Service {
-    public void validateInput(int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException("Negative numbers not allowed.");
-        }
-        // other logic
-    }
-}
