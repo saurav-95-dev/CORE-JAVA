b66d8244a40dfe8a696ddd9801f219a4acd82eed
Saurabh <devsaurabh086@gmail.com>
2025-12-23 23:32:30 +0530

    BeforeEach & AfterEach annotations

--- src/main/java/com/dev/learning/SpringbootCourse.java
+++
@@ -1,8 +0,0 @@
-package com.dev.learning;
-
-public class SpringbootCourse implements Course {
-    public boolean selectCourse() {
-        System.out.println("Springboot Course is purchased");
-        return true;
-    }
-}
--- src/test/java/com/dev/learning/TestBeforeAfter.java
+++ src/test/java/com/dev/learning/TestBeforeAfter.java
@@ -1,19 +1,33 @@
 package com.dev.learning;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
 class TestBeforeAfter {
+    Shapes s;
+    @BeforeEach
+    void init(){
+        s = new Shapes();
+        System.out.println("Before tests");
+    }
 
-    Shapes s = new Shapes();
     @Test
     void testSquareArea() {
-        Assertions.assertEquals(78.5, s.squareArea(5));
+        Assertions.assertEquals(25, s.squareArea(5));
+        System.out.println("square test running..");
     }
     @Test
     void testCircleArea() {
+       Assertions.assertEquals(78.5, s.circleArea(5));
+       System.out.println("circle test running..");
+    }
 
+    @AfterEach
+    void destroy(){
+        System.out.println("cleanup ...");
     }
 }
