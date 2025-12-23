960af044968cd468cb1d609595bd9c8048fe82a1
Saurabh <devsaurabh086@gmail.com>
2025-12-23 23:53:17 +0530

    BeforeAll & AfterAll annotaion--for common operational tasks

--- src/test/java/com/dev/learning/TestBeforeAfter.java
+++ src/test/java/com/dev/learning/TestBeforeAfter.java
@@ -1,14 +1,26 @@
 package com.dev.learning;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.Assert.assertEquals;
 
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TestBeforeAfter {
+
+    TestBeforeAfter(){
+        System.out.println("Constructor executed..");
+    }
+
     Shapes s;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before All tests");  //executed only once
+    }
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After All tests");
+    }
     @BeforeEach
     void init(){
         s = new Shapes();
