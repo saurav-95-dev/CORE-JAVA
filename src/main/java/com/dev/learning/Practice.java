ffbc28924ee77714148b88495808b2377772b1a9
Saurabh <devsaurabh086@gmail.com>
2025-12-24 20:41:38 +0530

    Selection-sort.java

--- src/main/java/com/dev/learning/Practice.java
+++ src/main/java/com/dev/learning/Practice.java
@@ -4,39 +4,35 @@ import java.util.Arrays;
 
 public class Practice{
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8};
+        int[] arr = {1,5,0,4,-1,11,6,6,0};
         int n = arr.length;
         System.out.println("Before sorting:");
         for(int i : arr){
             System.out.print(i+" ");
         }
         System.out.println();
-        bubbleSort(arr , n);
+        selectionSort(arr , n);
         System.out.println("After sorting:");
         for(int i : arr){
             System.out.print(i+" ");
         }
     }
 
-    public static void bubbleSort(int[] arr, int n){
+    public static void selectionSort(int[] arr, int n){
+        int temp;
+        for(int i=0;i<n;i++){
+            int minValIndex  = i;
+            //from this loop , we find the min value's index
+            for(int j = i+1;j<n;j++){
+                if(arr[j] < arr[minValIndex]){
+                    minValIndex = j; //update
+                }
+            }
+            temp = arr[i];
+            arr[i] = arr[minValIndex];
+            arr[minValIndex] = temp;
 
-         int temp = 0;
-         //outer loop is to track number of rounds.
-         for(int i = 0; i<=n-1;i++){
-             int flag = 0;
-             for(int j = 0;j<n-i-1;j++){
-                 if(arr[j]>arr[j+1]){
-                     temp = arr[j];
-                     arr[j] = arr[j+1];
-                     arr[j+1] = temp;
-                     flag = 1;
-                 }
-             }
-             if(flag==0){
-                 System.out.println("Number of rounds taken:" + i);
-                 break;
-             }
+        }
 
-         }
     }
 }
