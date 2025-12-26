63dfa86f9b0025f3e704c52ebc780c5d677349f1
Saurabh <devsaurabh086@gmail.com>
2025-12-26 23:52:37 +0530

    Initializing linked-list--Inbuilt

---
+++ src/main/java/com/dev/learning/Runner.java
@@ -0,0 +1,11 @@
+//Introduction to Linked-list :
+//ARRAY VS LINKED-LIST
+//Inbuilt implementation
+//Manual implementation
+package com.dev.learning;
+
+public class Runner {
+    public static void main(String[] args) {
+
+    }
+}
