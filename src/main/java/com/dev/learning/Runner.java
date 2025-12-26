2d4621a065a8286ae983485fe0b7c4a091ace201
Saurabh <devsaurabh086@gmail.com>
2025-12-27 00:57:34 +0530

    Linked-list - Manual implementation-add, remove , add at a position

--- src/main/java/com/dev/learning/Runner.java
+++ src/main/java/com/dev/learning/Runner.java
@@ -14,9 +14,8 @@ public class Runner {
         list.add(2);
         list.add(3);
         list.add(4);
-
-
-
-
+        list.add(2,12);
+        list.remove(4);
+        System.out.println(list);
     }
 }
