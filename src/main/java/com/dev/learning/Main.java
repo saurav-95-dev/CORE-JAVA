34287f46170d282bb8dc9fa0d48c17575ba6a362
Saurabh <devsaurabh086@gmail.com>
2025-12-28 19:53:58 +0530

    Improved selection-sort

--- src/main/java/com/dev/learning/Main.java
+++ src/main/java/com/dev/learning/Main.java
@@ -1,29 +1,30 @@
-//Selection-Sort:
-//Algo -> The idea is to select the minimum value and place it in its correct position.
-
+//Selection-sort :
 package com.dev.learning;
 
 public class Main{
-    public static void main(String[] args){
-        System.out.println("Selection-sort");
-        int[] arr = {3,2,5,4,7,9,8};
+    public static void main(String[] args) {
+        System.out.println("Selection sort");
+        int[] arr = {1,2,5,4,8,6,0,-1,-1,5,1};
+        //{0,1,2,5,4,7,6}
         int n = arr.length;
         for(int i=0;i<n;i++){
-            int minIndex   = i;
+            int minIndex = i;
             for(int j=i+1;j<n;j++){
                 if(arr[j]<arr[minIndex]){
                     minIndex = j;
                 }
             }
-            if (minIndex != i) {
+
+            //After this loop , minIndex has the index of minimum value in the array.
+            if(minIndex!=i){
                 int temp = arr[i];
                 arr[i] = arr[minIndex];
                 arr[minIndex] = temp;
             }
         }
-        for(int num:arr){
-            System.out.println(num);
+
+        for(int num : arr){
+            System.out.print(num + " ");
         }
     }
-
 }
