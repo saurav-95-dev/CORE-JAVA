d89d4ee938b34b9d61be56aca3da2e80057bde73
Saurabh <devsaurabh086@gmail.com>
2026-01-03 17:30:25 +0530

    Basic-Sorting-->Optimized Selection Sort

--- src/main/java/com/dev/learning/Main.java
+++ src/main/java/com/dev/learning/Main.java
@@ -1,24 +1,24 @@
 package com.dev.learning;
-//Algo -> The idea is to check the adjacent elements and do swapping .
-//-> After each round, largest element will be in the correct position.
+//Algo -> The idea is to identify the curr minimum from the array and do swaping with the assumed minimum.
+//After each inner loop processing , one element will be placed a correct position from the left side onwards.
 class Main{
     public static void main(String[] args){
-        System.out.println("Bubble sort:");
+        System.out.println("Selection sort:");
         //Eg -> {2,1,4,3,5,6,9,7}
-        int[] arr = {2,1,4,3,5,6,9,7};
+        int[] arr = {2,1,4,3,5,6,9,7,-1,0,1};
         int n = arr.length;
-        for(int i=0;i<n-1;i++){//Outer loop denotes number of rounds
-            boolean flag = false;
-            for(int j=0;j<n-i-1;j++){
-                //inner loop is to traverse through entire array and do processing for the current round.
-                if(arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                    flag = true;
+        for(int i=0;i<n-1;i++){
+            int minIndex  = i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
                 }
             }
-            if(!flag) break;
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
         }
         for(int i : arr){
             System.out.print(i+ " ");
