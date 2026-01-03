b3eed516c504153b3e67d86c1f0fa5456da04d5c
Saurabh <devsaurabh086@gmail.com>
2026-01-03 16:57:34 +0530

    Basic-Sorting-->optimized bubble-sort

--- src/main/java/com/dev/learning/Main.java
+++ src/main/java/com/dev/learning/Main.java
@@ -1,28 +1,27 @@
 package com.dev.learning;
-
-public class Main {
-    public static void main(String[] args) {
-        int[] arr = {2,4,3,1,0,7,9,8,-1,0};
+//Algo -> The idea is to check the adjacent elements and do swapping .
+//-> After each round, largest element will be in the correct position.
+class Main{
+    public static void main(String[] args){
+        System.out.println("Bubble sort:");
+        //Eg -> {2,1,4,3,5,6,9,7}
+        int[] arr = {2,1,4,3,5,6,9,7};
         int n = arr.length;
-
-        for (int i = 0; i < n-1; i++) {
+        for(int i=0;i<n-1;i++){//Outer loop denotes number of rounds
             boolean flag = false;
-
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
+            for(int j=0;j<n-i-1;j++){
+                //inner loop is to traverse through entire array and do processing for the current round.
+                if(arr[j] > arr[j+1]){
                     int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                     flag = true;
                 }
             }
-
-            // improvement: stop if already sorted
-            if (!flag) break;
+            if(!flag) break;
         }
-
-        for (int x : arr) {
-            System.out.print(x + " ");
+        for(int i : arr){
+            System.out.print(i+ " ");
         }
     }
 }
