eaf40697761702039f17e3d24326da35d0a2c868
Saurabh <devsaurabh086@gmail.com>
2026-01-07 23:06:42 +0530

    Node Class Created --Node.java

---
+++ src/main/java/com/dev/learning/Node.java
@@ -0,0 +1,13 @@
+//Node class creation and Node initialization :
+package com.dev.learning;
+
+class Node{
+    //data members:
+    public int data;
+    public Node next;
+    //constructor for initialising node
+    public Node(int data){
+        this.data = data;
+        this.next=null;
+    }
+}
