a3bca3c12dab46e8686b7c8f504718a7e5c2dd24
Saurabh <devsaurabh086@gmail.com>
2026-01-13 22:55:35 +0530

    JAVA --Stack-Basic-isEmpty(),size(),StackOverflow , StackUnderflow Implemented

--- src/main/java/com/dev/learning/Runner.java
+++ src/main/java/com/dev/learning/Runner.java
@@ -5,6 +5,7 @@ class Runner{
     public static void main(String[] args){
         System.out.println("Inside main function:");
         Stack stack = new Stack();
+        System.out.println("Is Stack empty : " + stack.isEmpty());
         stack.push(1);
         stack.push(2);
         stack.push(3);
@@ -17,6 +18,33 @@ class Runner{
         System.out.println();
         int s = stack.peek();
         System.out.println("Current element in the top (Peek): " + s);
+        //Implementing size and isEmpty :
+        System.out.println("Stack size : " + stack.size());
+        System.out.println("After pushing 3 more elements:");
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        stack.show();
+        System.out.println();
+        System.out.println("Stack size : " + stack.size());
+        System.out.println("Is Stack empty : " + stack.isEmpty());
+        stack.push(8);
+        System.out.println();
+        stack.show();
+        System.out.println();
+        stack.push(9);
+        System.out.println("Poping elements:");
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.show();
+        System.out.println();
+        stack.pop();
+        System.out.println();
 
     }
 }
--- src/main/java/com/dev/learning/Stack.java
+++ src/main/java/com/dev/learning/Stack.java
@@ -2,22 +2,43 @@ package com.dev.learning;
 
 public class Stack {
     int top = 0;
-    int[] arr = new  int[5];
+    int[] arr = new  int[7];
 
     public void push(int value){
-         arr[top] = value;
-         top++;
+        if(top == arr.length){
+            System.out.println("Stack is full");
+        }
+        else{
+            arr[top] = value;
+            top++;
+        }
     }
 
     public int pop(){
-        top--;
-        int data =  arr[top];
-        arr[top] = 0;
+        int data = 0;
+        if(isEmpty()){
+            System.out.println("Stack is empty , you cant pop further as the array size is :" + size());
+        }
+        else{
+            top--;
+            data =  arr[top];
+            arr[top] = 0;
+
+        }
         return data;
+
     }
 
     public int peek(){
         return arr[top-1];
+    }
+
+    public int size(){
+        return top;
+    }
+
+    public boolean isEmpty(){
+        return top <= 0;
     }
 
     public void show(){
