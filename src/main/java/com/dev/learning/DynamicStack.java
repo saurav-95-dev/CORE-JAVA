ed33f622f327b084841f0b232f835c186fdf0ca6
Saurabh <devsaurabh086@gmail.com>
2026-01-13 23:05:01 +0530

    Concept of Dynamic-Stack

---
+++ src/main/java/com/dev/learning/DynamicStack.java
@@ -0,0 +1,52 @@
+package com.dev.learning;
+
+public class DynamicStack {
+    int capacity  = 2;
+    int[] arr = new int[capacity];
+    int top = 0;
+
+    public void push(int value){
+        if(size() == capacity){
+            expand();
+        }
+            arr[top] = value;
+            top++;
+    }
+
+    private void expand(){
+
+    }
+
+    public int pop(){
+        int data = 0;
+        if(isEmpty()){
+            System.out.println("Stack is empty , you cant pop further as the array size is :" + size());
+        }
+        else{
+            top--;
+            data =  arr[top];
+            arr[top] = 0;
+
+        }
+        return data;
+
+    }
+
+    public int peek(){
+        return arr[top-1];
+    }
+
+    public int size(){
+        return top;
+    }
+
+    public boolean isEmpty(){
+        return top <= 0;
+    }
+
+    public void show(){
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+    }
+}
