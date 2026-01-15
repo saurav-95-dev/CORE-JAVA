e50c04248a990bb7c60d1e6a782d02c82bc71687
Saurabh <devsaurabh086@gmail.com>
2026-01-15 23:48:14 +0530

    JAVA--Queue Basic enQueue() Operation

--- src/main/java/com/dev/learning/Runner.java
+++ src/main/java/com/dev/learning/Runner.java
@@ -5,7 +5,9 @@ class Runner{
     public static void main(String[] args){
         System.out.println("Inside main function:");
         Queue queue = new Queue();
-
+        queue.enQueueu(1);
+        queue.enQueueu(2);
+        queue.show();
 
 
     }
