64a553fa3e48a6dfb37d24b5c1c0bbdc54788e40
Saurabh <devsaurabh086@gmail.com>
2026-01-17 23:41:28 +0530

    Baisc Circular Queue-Implementation

--- src/main/java/com/dev/learning/Queue.java
+++ src/main/java/com/dev/learning/Queue.java
@@ -3,26 +3,34 @@ package com.dev.learning;
 class Queue{
     //Static size/Fixed Sized
     int[] queue  = new int[5];
+    int n = queue.length;
     int front;
     int rear;
     int size;
     public void enQueue(int data){
         queue[rear] = data;
-        rear++;
+        rear = (rear + 1)%n;
         size++;
     }
 
     public int deQueue(){
         int  data = queue[front];
-        front++;
+        front =  (front + 1)%n;
         size--;
         return data;
     }
 
     public void show(){
         for(int i=0;i<size;i++){
-            System.out.print(queue[front + i]+" ");
+            System.out.print(queue[(front + i)%n]+" ");
+            //0+1 , 1+1 , 1+2 , 1+3 , 1+4 ->5
         }
+        System.out.println();
+        System.out.println("Elements inside actual array:");
+        for(int i : queue){
+            System.out.print(i+ " ");
+        }
+        System.out.println();
     }
 
 }
--- src/main/java/com/dev/learning/Runner.java
+++ src/main/java/com/dev/learning/Runner.java
@@ -10,11 +10,16 @@ class Runner{
         queue.enQueue(2);
         queue.enQueue(3);
         queue.enQueue(4);
-        System.out.println("Current Elements:");
+        System.out.println("Current Elements Inside queue after 4 enQueue():");
         queue.show();
+        System.out.println();
         System.out.println("Deleting one element-deQueue():");
         System.out.println(queue.deQueue());
-        System.out.println("Current Elements:");
+        System.out.println("Current Elements inside queue after one deQueue():");
+        queue.show();
+        queue.enQueue(5);
+        queue.enQueue(6);
+        System.out.println("Current Elements inside queue after again 2 enQueue():");
         queue.show();
 
 
