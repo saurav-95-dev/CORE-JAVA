0a2221ba213a808a260fd64ed42e00fde828fad0
Saurabh <devsaurabh086@gmail.com>
2026-01-18 19:56:30 +0530

    Edge Cases for Circular Queue--Underflow/Overflow

--- src/main/java/com/dev/learning/Queue.java
+++ src/main/java/com/dev/learning/Queue.java
@@ -8,17 +8,41 @@ public class Queue{
     int size = 0;
 
     public void enQueue(int data){
-        queue[rear % n] = data;
-        rear++;
-        size++;
+        if(isFull()){
+            System.out.println("Warning .... Queue is full");
+            return;
+        }
+        else{
+            queue[rear % n] = data;
+            rear++;
+            size++;
+        }
     }
 
     public int deQueue(){
-        int data = queue[front % n];
-        front++;
-        size--;
+        int data=0;
+        if(isEmpty()){
+            System.out.println("Warning ..Queue is empty");
+            return -1;
+        }
+        else{
+            data = queue[front % n];
+            front++;
+            size--;
+        }
         return data;
     }
+    public boolean isEmpty(){
+        return size==0;
+    }
+    public boolean isFull(){
+        return size == n;
+    }
+
+    public int getQueueSize(){
+        return size;
+    }
+
     public void show(){
         System.out.println("Elements inside queue:" );
         for(int i=0;i<size;i++){
--- src/main/java/com/dev/learning/Runner.java
+++ src/main/java/com/dev/learning/Runner.java
@@ -6,18 +6,32 @@ public class Runner{
         System.out.println("Static-Circular-Queue-Implementation");
         System.out.println();
         Queue queue = new Queue();
+        System.out.println("Empty Status:" + queue.isEmpty());
+        System.out.println("Full Status:" + queue.isFull());
         queue.enQueue(1);
         queue.enQueue(2);
         queue.enQueue(3);
         queue.enQueue(4);
         queue.enQueue(5);
-        System.out.println("Elements after 6 enQueue():");
+        System.out.println("Elements after 5 enQueue():");
+        System.out.println("Empty Status:" + queue.isEmpty());
+        System.out.println("Full Status:" + queue.isFull());
         queue.show();
         System.out.println();
         System.out.println("Elements after 1 deQueue()-> " + queue.deQueue());
+        System.out.println("Empty Status:" + queue.isEmpty());
+        System.out.println("Full Status:" + queue.isFull());
+        System.out.println("Boundary element added--------------");
         queue.enQueue(6);
         System.out.println("Elements after one enQueue() again:");
         queue.show();
+        System.out.println("Empty Status:" + queue.isEmpty());
+        System.out.println("Full Status:" + queue.isFull());
+        queue.enQueue(7);
+        System.out.println("Elements after inserting 7:");
+        queue.show();
+        System.out.println("Empty Status:" + queue.isEmpty());
+        System.out.println("Full Status:" + queue.isFull());
 
     }
 }
