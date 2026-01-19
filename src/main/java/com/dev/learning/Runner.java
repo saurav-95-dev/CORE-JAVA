b4b09590c42a7839cec53789745fde02a14d077a
Saurabh <devsaurabh086@gmail.com>
2026-01-19 22:29:54 +0530

    Stack-StaticArray Implementation-Push-Operation

--- src/main/java/com/dev/learning/Runner.java
+++ src/main/java/com/dev/learning/Runner.java
@@ -1,38 +1,15 @@
-//Dynamic-Circular-Queue-Implementation:
+//Stack-Implementation -- Basic:
 package com.dev.learning;
 
-public class Runner{
+class Runner{
     public static void main(String[] args){
-        System.out.println("Static-Circular-Queue-Implementation");
-        System.out.println();
-        DynamicCircularQueue queue = new  DynamicCircularQueue();
-        System.out.println("Empty Status:" + queue.isEmpty());
-        System.out.println("Full Status:" + queue.isFull());
-        queue.enQueue(1);
-        queue.enQueue(2);
-        queue.enQueue(3);
-        queue.enQueue(4);
-        queue.enQueue(5);
-        System.out.println("Elements after 5 enQueue():");
-        System.out.println("Empty Status:" + queue.isEmpty());
-        System.out.println("Full Status:" + queue.isFull());
-        queue.show();
-        System.out.println();
-        System.out.println("Elements after 1 deQueue()-> " + queue.deQueue());
-        System.out.println("Empty Status:" + queue.isEmpty());
-        System.out.println("Full Status:" + queue.isFull());
-        System.out.println("Boundary element added--------------");
-        queue.enQueue(6);
-        System.out.println("Elements after one enQueue() again:");
-        queue.show();
-        System.out.println("Empty Status:" + queue.isEmpty());
-        System.out.println("Full Status:" + queue.isFull());
-        queue.enQueue(7);
-        System.out.println("Elements after inserting 7:");
-        queue.show();
-        System.out.println("Empty Status:" + queue.isEmpty());
-        System.out.println("Full Status:" + queue.isFull());
-
-
+        System.out.println("Inside main function:");
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.show();
     }
 }
--- src/main/java/com/dev/learning/Stack.java
+++ src/main/java/com/dev/learning/Stack.java
@@ -1,49 +1,16 @@
 package com.dev.learning;
 
-public class Stack {
-    int top = 0;
-    int[] arr = new  int[7];
+public class Stack{
+    int[] arr = new int[5];
+    int top  = 0;
 
-    public void push(int value){
-        if(top == arr.length){
-            System.out.println("Stack is full");
-        }
-        else{
-            arr[top] = value;
-            top++;
-        }
+    public void push(int data){
+        arr[top] = data;
+        top++;
     }
-
-    public int pop(){
-        int data = 0;
-        if(isEmpty()){
-            System.out.println("Stack is empty , you cant pop further as the array size is :" + size());
-        }
-        else{
-            top--;
-            data =  arr[top];
-            arr[top] = 0;
-
-        }
-        return data;
-
-    }
-
-    public int peek(){
-        return arr[top-1];
-    }
-
-    public int size(){
-        return top;
-    }
-
-    public boolean isEmpty(){
-        return top <= 0;
-    }
-
     public void show(){
-          for(int i : arr){
-              System.out.print(i + " ");
-          }
+         for(int i : arr){
+             System.out.print(i + " ");
+         }
     }
 }
