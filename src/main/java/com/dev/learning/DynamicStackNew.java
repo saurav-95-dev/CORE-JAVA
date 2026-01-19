baa2f5e0bc8f22facaadc478d0b1f5e592f42918
Saurabh <devsaurabh086@gmail.com>
2026-01-19 23:14:43 +0530

    DynamicStack ShrinkCapacity Implemented

--- src/main/java/com/dev/learning/DynamicStackNew.java
+++ src/main/java/com/dev/learning/DynamicStackNew.java
@@ -1,51 +1,74 @@
-//Dynamic Stack Implementation:
 package com.dev.learning;
 
-public class DynamicStackNew{
+public class DynamicStackNew {
+
     int capacity = 5;
-    int top  = 0;
+    final int MIN_CAPACITY = 5;
+    int top = 0;
     int[] arr = new int[capacity];
 
     public void push(int data){
         if(isFull()){
-            System.out.println("Stack is full , hence need to call expand()");
             expand();
         }
-        arr[top] = data;
-        top++;
+        arr[top++] = data;
     }
-    public void expand(){
+
+    private void expand(){
         capacity *= 2;
         int[] newArr = new int[capacity];
-        System.arraycopy(arr,0,newArr,0,top);
+        System.arraycopy(arr, 0, newArr, 0, top);
         arr = newArr;
     }
+
     public int pop(){
         if(isEmpty()){
             System.out.println("Stack is empty");
             return -1;
         }
-        top--;
-        int data =  arr[top];
+
+        int data = arr[--top];
         arr[top] = 0;
+
+        // shrink AFTER pop
+        if(top <= capacity / 4 && capacity > MIN_CAPACITY){
+            shrink();
+        }
+
         return data;
     }
+
+    private void shrink(){
+        capacity /= 2;
+        int[] newArr = new int[capacity];
+        System.arraycopy(arr, 0, newArr, 0, top);
+        arr = newArr;
+    }
+
     public int peek(){
         if(isEmpty()){
             System.out.println("Stack is empty");
             return -1;
         }
-        return arr[top-1];
+        return arr[top - 1];
     }
+
     public int getSize(){
         return top;
     }
+
+    public int getCapacity(){
+        return capacity;
+    }
+
     public boolean isEmpty(){
         return top == 0;
     }
+
     public boolean isFull(){
         return top == capacity;
     }
+
     public void show(){
         for(int i : arr){
             System.out.print(i + " ");
