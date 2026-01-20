27c7e82f20e8c8a288adcf563172c51c31b19b13
Saurabh <devsaurabh086@gmail.com>
2026-01-20 23:18:55 +0530

    String-Immutability

--- src/main/java/com/dev/learning/Demo.java
+++ src/main/java/com/dev/learning/Demo.java
@@ -4,13 +4,15 @@
 package com.dev.learning;
 
 public class Demo{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Inside main method:");
         String s = new String("Hello world");
         System.out.println(s); //Blank object inside heap
         String s1 = "Learning Core-Java";
         System.out.println(s1);
-
+        s1 = s1 + " Concatination"; //s1 is same , here a new object is created in string constant pool in heap at a separate location
+        System.out.println(s1);
+        //Previous unchanged s1 is eligible for garbage collection.
 
     }
 }
