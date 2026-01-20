b5c9294dc45e8611da1314440a10c9ad6b44a456
Saurabh <devsaurabh086@gmail.com>
2026-01-20 23:24:27 +0530

    Space Utilization through String constant pool

--- src/main/java/com/dev/learning/Demo.java
+++ src/main/java/com/dev/learning/Demo.java
@@ -8,11 +8,15 @@ public class Demo{
         System.out.println("Inside main method:");
         String s = new String("Hello world");
         System.out.println(s); //Blank object inside heap
-        String s1 = "Learning Core-Java";
+        String s1 = "Learning Core-Java"; //Object created in heap
         System.out.println(s1);
-        s1 = s1 + " Concatination"; //s1 is same , here a new object is created in string constant pool in heap at a separate location
+        s1 = s1 + " Concatination"; //s1 is same , here a new object (the variable will points towards different address , previous value will remain same in older address)is created in string constant pool in heap at a separate location
         System.out.println(s1);
         //Previous unchanged s1 is eligible for garbage collection.
 
+        String d1 = "Dev";
+        String d2 = "Dev";
+        System.out.println(d1 == d2); //Effective Space Utilization via String constant pool.
+        //both d1 , d2 use the same address
     }
 }
