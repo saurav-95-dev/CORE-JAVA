ba78663fe32606525819568b24b3021837af704a
Saurabh <devsaurabh086@gmail.com>
2026-01-20 23:15:13 +0530

    Generic way of defining Strings

--- src/main/java/com/dev/learning/Demo.java
+++ src/main/java/com/dev/learning/Demo.java
@@ -6,8 +6,11 @@ package com.dev.learning;
 public class Demo{
     public static void main(String[] args){
         System.out.println("Inside main method:");
-        String s = new String();
+        String s = new String("Hello world");
         System.out.println(s); //Blank object inside heap
+        String s1 = "Learning Core-Java";
+        System.out.println(s1);
+
 
     }
 }
