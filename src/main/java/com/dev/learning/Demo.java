2eb7330700ce8a22af2494dd626343c272323a7f
Saurabh <devsaurabh086@gmail.com>
2026-01-20 23:13:41 +0530

    Defining Strings

---
+++ src/main/java/com/dev/learning/Demo.java
@@ -0,0 +1,13 @@
+//REV-String :
+//Note : Strings --> Immutable in java by default
+
+package com.dev.learning;
+
+public class Demo{
+    public static void main(String[] args){
+        System.out.println("Inside main method:");
+        String s = new String();
+        System.out.println(s); //Blank object inside heap
+
+    }
+}
