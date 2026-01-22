d1e65e99ae97a222475f053951e5326a20f288b0
Saurabh <devsaurabh086@gmail.com>
2026-01-22 22:29:24 +0530

    StringBuffer--Capacity

--- src/main/java/com/dev/learning/Demo.java
+++ src/main/java/com/dev/learning/Demo.java
@@ -5,18 +5,10 @@ package com.dev.learning;
 
 public class Demo{
     public static void main(String[] args) {
-        System.out.println("Inside main method:");
-        String s = new String("Hello world");
-        System.out.println(s); //Blank object inside heap
-        String s1 = "Learning Core-Java"; //Object created in heap
-        System.out.println(s1);
-        s1 = s1 + " Concatination"; //s1 is same , here a new object (the variable will points towards different address , previous value will remain same in older address)is created in string constant pool in heap at a separate location
-        System.out.println(s1);
-        //Previous unchanged s1 is eligible for garbage collection.
+        //Strings in java are immutable by default.
+        //Strings can be made mutable by -> StringBuffer and StringBuilder.
+        StringBuffer sb  = new StringBuffer();
+        System.out.println(sb.capacity());
 
-        String d1 = "Dev";
-        String d2 = "Dev";
-        System.out.println(d1 == d2); //Effective Space Utilization via String constant pool.
-        //both d1 , d2 use the same address
     }
 }
