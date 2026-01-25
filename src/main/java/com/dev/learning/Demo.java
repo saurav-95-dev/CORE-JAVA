e2d6a9fd41a2e2bce31414016c1ffe077802946e
Saurabh <devsaurabh086@gmail.com>
2026-01-25 13:33:07 +0530

    Static-Keyword Glimpse -- All in one

--- src/main/java/com/dev/learning/Demo.java
+++ src/main/java/com/dev/learning/Demo.java
@@ -1,57 +1,43 @@
-//All about static variable ---> CONTENTS:
-//->static keyword/variable , static block.
-//->static/non-static methods, instance VS static variable, static variable behaviour inside constructor.
-//->static members execution flow and class loader.
-//->Variable cerated inside class will be called as instance variable.
-//->Variable created inside method will be referred as local variable.
-//->Static variables are shared by all the objects & they belong to a class not object and hence can be accessed via className.
-//CONCEPT - We can initialise a static variable inside a constructor .
-/*Problem with above - Everytime an object gets created, constructor will get called initialising the instance variable
-which is fine.But it will also initialise the static variable which is not the part of the object .
-Hence , static variable should be initialised inside static block(runs only once)
- */
-/*Whenever object is create -->1)Class loads first(static block get called) , 2)Object gets initialised
-So that means that if we don't have any object, both constructor and static block will not get called.
+//Static keyword all concept :
+//Keys->
+/*
+->static members belongs to class not object unlike instance variables.
+->In order to call a non-static method , we require object of class .Hence we make main method static.
+->static members should be initialised under static block(which runs only once ) not  in constructors
  */
 package com.dev.learning;
 
 class Mobile{
-    //instance variable
     int price;
-    String company;
+    String name;
     static String device;
 
-    //constructor:
-    public Mobile(int price, String company) {
-        this.price = price;
-        this.company = company;
-        System.out.println("Constructor runs..");
-    }
-
     static{
-        device = "Android";
-        System.out.println("Static block runs..");
+        device = "universal";
     }
-
+    //constructor:
+    public Mobile(int price, String name) {
+        this.price = price;
+        this.name = name;
+    }
     public void show(){
-        System.out.println("Price:"+ price + "  Company:"+ company + "  Device-Type:"+ device);
+        System.out.println("Price:" + price + "  Name:" + name + "  Device:" + device);
     }
-
 }
-public class Demo{
-    public static void main(String[] args) throws ClassNotFoundException {
-        Class.forName("com.dev.learning.Mobile"); //--> Class.forName() requires the fully qualified class name (package + class name).
-         // Mobile mobile1 = new Mobile(12 , "Sapient");
-//          mobile1.price = 100;
-//          mobile1.company = "Samsung";
-//          Mobile.device = "ipad";
-          //mobile1.show();
-          //Mobile mobile2 = new Mobile(13 , "Deloitte");
-//          mobile2.price = 200;
-//          mobile2.company = "Motorola";
-//          Mobile.device = "ipad";
-         // mobile2.show();
-
-
+class Demo{
+    public static void main(String[] args){
+        System.out.println("Static - All in one:");
+        Mobile mobile1 = new Mobile(12 , "motorola");
+        mobile1.price = 122;
+        mobile1.name = "iphone";
+        mobile1.show();
+        System.out.println();
+        Mobile mobile2 = new Mobile(11 , "Apple");
+        mobile2.show();
+        //Changing the default value of static variable defined in static block.
+        System.out.println();
+        Mobile.device = "Universal was changed for both mobiles";
+        mobile2.show();
+        mobile1.show();
     }
 }
