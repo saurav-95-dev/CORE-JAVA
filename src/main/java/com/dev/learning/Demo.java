a69a467848623ed8a68865769b0e957dbc9d17a0
Saurabh <devsaurabh086@gmail.com>
2026-01-25 13:59:12 +0530

    Order of Execution - Static VS Constructor Blocks

--- src/main/java/com/dev/learning/Demo.java
+++ src/main/java/com/dev/learning/Demo.java
@@ -3,7 +3,12 @@
 /*
 ->static members belongs to class not object unlike instance variables.
 ->In order to call a non-static method , we require object of class .Hence we make main method static.
-->static members should be initialised under static block(which runs only once ) not  in constructors
+->static members should be initialised under static block(which runs only once) not  in constructors.
+->Whenever an object is created ->1)class gets loads first and then object gets initialised.
+->When the class gets loaded , static blocks runs and after that object gets initialised.
+->No object -> no constructor runs , no static blocks runs.
+->If you wanna call static block without creation of any object:
+a)Use - Class.forName("name of the class").
  */
 package com.dev.learning;
 
@@ -14,19 +19,24 @@ class Mobile{
 
     static{
         device = "universal";
+        System.out.println("Static block of Mobile class running..");
     }
     //constructor:
     public Mobile(int price, String name) {
         this.price = price;
         this.name = name;
+        System.out.println("Constructor called after static block during object creation.");
     }
     public void show(){
         System.out.println("Price:" + price + "  Name:" + name + "  Device:" + device);
     }
 }
 class Demo{
+    static{
+        System.out.println("Static block of main method running...");
+    }
     public static void main(String[] args){
-        System.out.println("Static - All in one:");
+
         Mobile mobile1 = new Mobile(12 , "motorola");
         mobile1.price = 122;
         mobile1.name = "iphone";
@@ -39,5 +49,6 @@ class Demo{
         Mobile.device = "Universal was changed for both mobiles";
         mobile2.show();
         mobile1.show();
+        //Class.forName("com.dev.learning.Mobile");
     }
 }
