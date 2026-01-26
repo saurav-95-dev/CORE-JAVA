de5f700e30c1a71aa59d50a4fddf1db1e326d453
Saurabh <devsaurabh086@gmail.com>
2026-01-26 18:21:59 +0530

    JDBC-Implementation-Blue-Print

--- src/main/java/com/dev/learning/DemoClass.java
+++ src/main/java/com/dev/learning/DemoClass.java
@@ -15,10 +15,21 @@ import java.sql.*;
 
 
 class DemoClass {
-    public static void main(String[] args) throws ClassNotFoundException {
-        Class.forName("com.mysql.jdbc.Driver"); //Responsible for loading the class.
-
-
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://localhost:3306/org";
+        String username = "root";
+        String password = "";
+        String query  = "select * from worker where Worker_id = 1";
+        Class.forName("com.mysql.cj.jdbc.Driver"); //Responsible for loading the class.
+        Connection con = DriverManager.getConnection(url , username,password);
+        Statement st  =  con.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        rs.next();
+        String name =  rs.getString(username);
+        System.out.println(name);
+        st.close();
+        con.close();
+        
 
     }
 }
