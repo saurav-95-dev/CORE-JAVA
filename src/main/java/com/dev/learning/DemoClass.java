17c3fe56a586539bebc4526a552ded8c01d014a2
Saurabh <devsaurabh086@gmail.com>
2026-01-26 18:51:47 +0530

    Fetching Multiple Rows from MySQL

--- src/main/java/com/dev/learning/DemoClass.java
+++ src/main/java/com/dev/learning/DemoClass.java
@@ -7,17 +7,20 @@ class DemoClass {
         String url = "jdbc:mysql://localhost:3306/org";
         String username = "root";
         String password = "Saurabh@123"; // your real password
-        String query = "select * from worker where Worker_id = 2";
+        String query = "select * from worker";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
 
         Connection con = DriverManager.getConnection(url, username, password);
         Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery(query);
+        ResultSet rs = st.executeQuery("select * from worker");
 
-        if (rs.next()) {
-            String firstName = rs.getString("first_name");
-            System.out.println(firstName);
+        while (rs.next()) {
+            System.out.println(
+                    rs.getInt("Worker_id") + " " +
+                            rs.getString("first_name") + " " +
+                            rs.getString("last_name")
+            );
         }
 
         rs.close();
