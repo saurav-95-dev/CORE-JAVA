dedf9daf7a94ff4ee61461da7a9caafe47d18efa
Saurabh <devsaurabh086@gmail.com>
2026-01-26 13:40:42 +0530

    Concept Of this Keyword To Access Instance Variable

--- src/main/java/com/dev/learning/Demo.java
+++ src/main/java/com/dev/learning/Demo.java
@@ -1,33 +1,27 @@
-//Encapsulation:
-//Keys :
+//Concept of "this" keyword :
 package com.dev.learning;
-class Human{
-    private int age;
-    private String name;
 
-    public int getAge(){
-        return age;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setAge(int age){
-        this.age = age;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
+class Student{
+    public String name;
+    public int marks;
 
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+    public int getMarks() {
+        return marks;
+    }
+    public void setName(String name) {
+        this.name = name; //this is a pointer to the student object .
+        //value of local variable name is assigned to instance variable i.e name using this keyword.
+        //In-short , we are accessing the instance variable via this keyword which points to the current object.
+    }
 }
-
 class Demo{
-    public static void main(String[] args) {
-        System.out.println("Encapsulation:");
-        Human h = new Human();
-        h.setAge(18);
-        h.setName("new name");
-        System.out.println( h.getAge());
-        System.out.println(h.getName());
-
+    public static void main(String[] args){
+        System.out.println("this keyword:");
+        Student student = new Student();
+        student.setMarks(12);
+        student.setName("Saurabh");
     }
 }
