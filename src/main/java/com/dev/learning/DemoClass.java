53aaece25fc637a8c5cf51131c9be7ffec0f6ab4
Saurabh <devsaurabh086@gmail.com>
2026-01-26 18:42:50 +0530

    JDBC-Connection Esatablished for ORG DB

--- src/main/java/com/dev/learning/DemoClass.java
+++ src/main/java/com/dev/learning/DemoClass.java
@@ -1,36 +1,27 @@
-//7 Steps to connect JAVA Application with the Database.
-/*
-1->Import the package. --> java.sql.*
-2->Load & Register the driver.
-a)Load the driver. -->driver for mysql is : com.mysql.jdbc.driver
-b)Register the driver. --> forName("com.mysql.jdbc.driver")
-3->Establish the connection.-->instantiate the 'Connection' Interface
-4->Create the Connection. --> Normal statement , prepare statement , callable statement
-5->Execute the query.
-6->Process the Result.
-7->Close.
-*/
 package com.dev.learning;
 import java.sql.*;
 
-
 class DemoClass {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws Exception {
+
         String url = "jdbc:mysql://localhost:3306/org";
         String username = "root";
-        String password = "";
-        String query  = "select * from worker where Worker_id = 1";
-        Class.forName("com.mysql.cj.jdbc.Driver"); //Responsible for loading the class.
-        Connection con = DriverManager.getConnection(url , username,password);
-        Statement st  =  con.createStatement();
+        String password = "Saurabh@123"; // your real password
+        String query = "select * from worker where Worker_id = 2";
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection con = DriverManager.getConnection(url, username, password);
+        Statement st = con.createStatement();
         ResultSet rs = st.executeQuery(query);
-        rs.next();
-        String name =  rs.getString(username);
-        System.out.println(name);
+
+        if (rs.next()) {
+            String firstName = rs.getString("first_name");
+            System.out.println(firstName);
+        }
+
+        rs.close();
         st.close();
         con.close();
-        
-
     }
 }
-
