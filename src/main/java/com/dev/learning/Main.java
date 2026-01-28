898bfffe911c9c157fd9ad570d54140d7f88220b
Saurabh <devsaurabh086@gmail.com>
2026-01-28 23:40:43 +0530

    DemoClass List Creation of Workers

--- src/main/java/com/dev/learning/Main.java
+++ src/main/java/com/dev/learning/Main.java
@@ -1,33 +1,46 @@
 package com.dev.learning;
 import java.sql.*;
+import java.util.*;
 
 class DemoClass {
     public static void main(String[] args) throws Exception {
 
         String url = "jdbc:mysql://localhost:3306/org";
         String username = "root";
-        String password = "Saurabh@123"; // your real password
-        String query = "select * from worker";
+        String password = "Saurabh@123";
+        String query = "SELECT * FROM worker";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
-
         Connection con = DriverManager.getConnection(url, username, password);
         Statement st = con.createStatement();
-        query = "SELECT * FROM worker";
-
         ResultSet rs = st.executeQuery(query);
 
-        while (rs.next()) {
-            int id = rs.getInt("Worker_id");
-            String firstName = rs.getString("first_name");
-            String lastName = rs.getString("last_name");
-            int salary = rs.getInt("salary");
-            String department = rs.getString("department");
+        List<Worker> workers = new ArrayList<>();
 
+        while (rs.next()) {
+            Worker worker = new Worker(
+                    rs.getInt("Worker_id"),
+                    rs.getString("first_name"),
+                    rs.getString("last_name"),
+                    rs.getInt("salary"),
+                    rs.getString("department")
+            );
+            workers.add(worker);
+        }
+
+        // print data
+        for (Worker w : workers) {
             System.out.println(
-                    id + " | " + firstName + " | " + lastName + " | " + salary + " | " + department
+                    w.getWorkerId() + " | " +
+                            w.getFirstName() + " | " +
+                            w.getLastName() + " | " +
+                            w.getSalary() + " | " +
+                            w.getDepartment()
             );
         }
 
+        rs.close();
+        st.close();
+        con.close();
     }
 }
