be5144125cb581d2257f32e0b0283732013c2392
Saurabh <devsaurabh086@gmail.com>
2026-01-29 23:49:36 +0530

    Inserting Data Into Table through JAVA

--- src/main/java/com/dev/learning/Demo.java
+++ src/main/java/com/dev/learning/Demo.java
@@ -1,4 +1,5 @@
 //JAVA-JDBC :
+//Inserting value into DB:
 package com.dev.learning;
 
 import java.sql.*;
@@ -8,16 +9,19 @@ class Demo{
         String url = "jdbc:mysql://localhost:3306/org";
         String username = "root";
         String password = "Saurabh@123";
-        String query = "Select * from Worker";
+        String query = "INSERT INTO worker (first_name, last_name, salary, joining_date, department)\n" +
+                "VALUES \n" +
+                "  ('NEW', 'COMER', 2122, '2020-10-21 09:00:00', 'JAVA-DEVELOPER')";
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection(url , username , password);
         Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery(query);
-        String userData = "";
-        while(rs.next()){
-            userData = rs.getInt(1) + " : " + rs.getString(2);
-            System.out.println(userData);
-        }
+        int count  = st.executeUpdate(query);
+//        String userData = "";
+//        while(rs.next()){
+//            userData = rs.getInt(1) + " : " + rs.getString(2);
+//            System.out.println(userData);
+//        }
+        System.out.println(count + " Rows Affected");
 
         st.close();
         con.close();
