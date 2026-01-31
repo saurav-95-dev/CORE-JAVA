d0d5a376753d80fd0f82a742581b60b5f17c8afa
Saurabh <devsaurabh086@gmail.com>
2026-01-31 22:09:48 +0530

    JDBC-REV--Coonecting JAVA with JDBC

---
+++ src/main/java/com/dev/learning/DemoClass.java
@@ -0,0 +1,21 @@
+//JDBC-Core Concepts-REV:
+//STEPS TO CONNECT JAVA APPLICATION TO THE DB:
+/*
+1->Import the package -->java.sql.*
+2->Load & Register the driver.
+a)Load driver - For mysql , we need driver from mysql-connector
+b)Register the driver - Class.forName("name of the driver")
+3->Establish Connection. - Use the interface called Connection.
+4->Create the statement. (statement , prepared statement , callable statement)
+5->Execute the query.
+6->Process the Result.
+7->Close
+*/
+
+package com.dev.learning;
+
+class DemoClass{
+    public static void main(String[] args) {
+
+    }
+}
