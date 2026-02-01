b60b876bebc52c4ab24cee2c5e8dba02c2961694
Saurabh <devsaurabh086@gmail.com>
2026-02-01 21:40:19 +0530

    JDBC--Inserting Values Inside DB Using Normal Statement

--- src/main/java/com/dev/learning/DemoClass.java
+++ src/main/java/com/dev/learning/DemoClass.java
@@ -10,14 +10,19 @@ class DemoClass{
         String username = "root";
         String password = "Saurabh@123";
         String query = "select * from worker";
+        String insertQuery = "INSERT INTO worker (first_name, last_name, salary, joining_date, department)\n" +
+                "VALUES \n" +
+                "  ('Mr', 'Dev', 30000, '2020-10-21 09:00:00', 'Java Developer')";
         String url = "jdbc:mysql://localhost:3306/org";
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection(url, username, password);
         Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery(query);
-        while(rs.next()){
-            System.out.println(rs.getInt(1) +" : " + rs.getString(2)+"  "+rs.getString(3));
-        }
+        //ResultSet rs = st.executeQuery(query);
+        int numberOfRowsEffected = st.executeUpdate(insertQuery);
+//        while(rs.next()){
+//            System.out.println(rs.getInt(1) +" : " + rs.getString(2)+"  "+rs.getString(3));
+//        }
+        System.out.println(numberOfRowsEffected + " rows affected.");
         st.close();
         con.close();
     }
