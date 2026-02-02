3f7a27aa83449793ae12af0435e9a24667e8f0d7
Saurabh <devsaurabh086@gmail.com>
2026-02-02 16:57:42 +0530

    Inserting Data via JDBC-Dao-Design-Pattern in DB

--- src/main/java/com/dev/learning/JdbcDaoDemo.java
+++ src/main/java/com/dev/learning/JdbcDaoDemo.java
@@ -3,20 +3,33 @@
 ->It is a design pattern that separates database logic from business logic.
 ->DAO is basically a class whose only job is to talk to the database.
 */
+//FETCHING , ADDING , AND REMOVING DATA:
 package com.dev.learning;
 
 import java.sql.*;
 
 class JdbcDaoDemo {
     public static void main(String[] args) throws Exception {
-        System.out.println("DAO-Design Pattern");
         StudentDao studentDao = new StudentDao();
-        Student s1 = studentDao.getStudent(1);
+        Student s1 = studentDao.getStudent(67);
         System.out.println(s1.first_name);
+        System.out.println("Adding a new student entry:");
+        Student s2 = new Student();
+        s2.first_name = "John";
+        s2.worker_id = 68;
+        studentDao.connect();
+        studentDao.addStudent(s2);
     }
 }
 
 class StudentDao{
+
+    Connection con = null;
+
+    public void connect() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con =  DriverManager.getConnection("jdbc:mysql://localhost:3306/org", "root", "Saurabh@123");
+    }
     public Student getStudent(int worker_id) throws Exception {
         String query = "select * from worker where worker_id = " +  worker_id;
         Student s = new Student();
@@ -27,9 +40,15 @@ class StudentDao{
         ResultSet rs = st.executeQuery(query);
         rs.next();
         s.first_name = rs.getString("first_name");
-
-
         return s;
+    }
+
+    public void addStudent(Student s) throws Exception {
+        String query = "insert into worker(worker_id,first_name) values (?,?)";
+        PreparedStatement p = con.prepareStatement(query);
+        p.setInt(1, s.worker_id);
+        p.setString(2, s.first_name);
+        p.executeUpdate();
     }
 }
 
