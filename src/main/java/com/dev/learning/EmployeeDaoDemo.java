b0c9f743f7a07f07ff72a4e14351d6a78bd3daa8
Saurabh <devsaurabh086@gmail.com>
2026-02-02 21:19:12 +0530

    Complete Insert Operation for Employee Table--DAO-JDBC

--- src/main/java/com/dev/learning/EmployeeDaoDemo.java
+++ src/main/java/com/dev/learning/EmployeeDaoDemo.java
@@ -13,6 +13,10 @@ class EmployeeDaoDemo{
         e.connect(); //First make connection of this dao object with DB.
         Employee s1 = e.getEmployee(2);
         System.out.println(s1.first_name + " " + s1.last_name);
+        //Adding a new record:
+        System.out.println("Adding a new employee:");
+        Employee s2 = e.addEmployee(70 , "Bhoot" , "Nath");
+        System.out.println(s2.first_name + " " + s2.last_name + " has been added");
     }
 }
 
@@ -39,6 +43,20 @@ class EmployeeDao{
         rs.close();
         ps.close();
 
+        return e;
+    }
+
+    public Employee addEmployee(int worker_id , String first_name , String last_name) throws Exception {
+        Employee e = new Employee();
+        e.worker_id = worker_id;
+        e.first_name = first_name;
+        e.last_name = last_name;
+        String query = "insert into worker (first_name, last_name) values (?, ?)";
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.setString(1, first_name);
+        ps.setString(2, last_name);
+        ps.executeUpdate();
+        ps.close();
         return e;
     }
 }
