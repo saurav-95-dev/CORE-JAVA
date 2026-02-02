ce6394cb304a54ca76fa1d7481f944b3f61354b3
Saurabh <devsaurabh086@gmail.com>
2026-02-02 20:54:19 +0530

    Initial-DAO-Template for Employee Table--Fetch Operation

--- src/main/java/com/dev/learning/EmployeeDaoDemo.java
+++ src/main/java/com/dev/learning/EmployeeDaoDemo.java
@@ -1,2 +1,16 @@
+//DAO-Operations - Fetch , Add , Manipulate , Delete
+/*
+->There will be a class in which all database-logic will be there.
+->
+ */
 package com.dev.learning;
 
+class EmployeeDaoDemo{
+    public static void main(String[] args){
+        EmployeeDao e = new EmployeeDaoDemo();
+        e.connect(); //First make connection of this dao object with DB.
+        Employee s1 = e.getEmployee(2);
+        System.out.println(s1.first_name);
+
+    }
+}
