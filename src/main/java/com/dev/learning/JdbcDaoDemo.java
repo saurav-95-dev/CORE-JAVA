7aa97f8a3ff10a3dd6cc8bb9c0cc54bb509f7352
Saurabh <devsaurabh086@gmail.com>
2026-02-02 15:03:58 +0530

    Accessing DB Data via JDBC-DAO Pattern

--- src/main/java/com/dev/learning/JdbcDaoDemo.java
+++ src/main/java/com/dev/learning/JdbcDaoDemo.java
@@ -5,26 +5,36 @@
 */
 package com.dev.learning;
 
-import java.sql.SQLOutput;
+import java.sql.*;
 
 class JdbcDaoDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("DAO-Design Pattern");
         StudentDao studentDao = new StudentDao();
-        Student s1 = studentDao.getStudent(2);
-        System.out.println(s1.worker_id);
+        Student s1 = studentDao.getStudent(1);
+        System.out.println(s1.first_name);
     }
 }
 
 class StudentDao{
-    public Student getStudent(int worker_id) {
+    public Student getStudent(int worker_id) throws Exception {
+        String query = "select * from worker where worker_id = " +  worker_id;
         Student s = new Student();
         s.worker_id = worker_id;
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con =  DriverManager.getConnection("jdbc:mysql://localhost:3306/org", "root", "Saurabh@123");
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        rs.next();
+        s.first_name = rs.getString("first_name");
+
+
         return s;
     }
 }
 
 class Student{
+    //Contains 2 attributes:
     String first_name;
     int worker_id;
 }
