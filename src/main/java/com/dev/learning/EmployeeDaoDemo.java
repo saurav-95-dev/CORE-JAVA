a2a7dbbbd4f5a39d1f288e8ec71de222d0faace5
Saurabh <devsaurabh086@gmail.com>
2026-02-02 23:19:14 +0530

    JDBC--Deleting a Record From Employee Table & Displaying Deleted Record

--- src/main/java/com/dev/learning/EmployeeDaoDemo.java
+++ src/main/java/com/dev/learning/EmployeeDaoDemo.java
@@ -17,6 +17,14 @@ class EmployeeDaoDemo{
         System.out.println("Adding a new employee:");
         Employee s2 = e.addEmployee(70 , "Bhoot" , "Nath");
         System.out.println(s2.first_name + " " + s2.last_name + " has been added");
+        System.out.println("Changing existing employee:");
+        Employee updated = e.updateEmployee(70, "Ghost", "King");
+        System.out.println("Updated Employee: " + updated.first_name + " " + updated.last_name);
+        System.out.println("Deleting a record:");
+        Employee rm = e.removeEmployee(4);
+        System.out.println(rm.first_name + " " + rm.last_name + " has been deleted");
+
+
     }
 }
 
@@ -57,6 +65,43 @@ class EmployeeDao{
         ps.setString(2, last_name);
         ps.executeUpdate();
         ps.close();
+        return e;
+    }
+    public Employee updateEmployee(int worker_id, String first_name, String last_name) throws Exception {
+
+        String query = "UPDATE worker SET first_name = ?, last_name = ? WHERE worker_id = ?";
+        PreparedStatement ps = con.prepareStatement(query);
+
+        ps.setString(1, first_name);
+        ps.setString(2, last_name);
+        ps.setInt(3, worker_id);
+
+        int rows = ps.executeUpdate();
+
+        ps.close();
+
+        Employee e = new Employee();
+        e.worker_id = worker_id;
+        e.first_name = first_name;
+        e.last_name = last_name;
+
+        if (rows == 0) {
+            System.out.println("No employee found with worker_id = " + worker_id);
+        }
+
+        return e;
+    }
+
+    public Employee removeEmployee(int worker_id) throws Exception {
+        //fetch employee before deletion:
+        Employee e = getEmployee(worker_id);
+        e.worker_id = worker_id;
+        //Now deleting employee:
+        String query = "delete from worker where worker_id = ?";
+        PreparedStatement st = con.prepareStatement(query);
+        st.setInt(1, worker_id);
+        int rows = st.executeUpdate();
+        System.out.println(rows + " row/s has been deleted");
         return e;
     }
 }
