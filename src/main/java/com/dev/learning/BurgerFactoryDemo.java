8d1ca6d92800bb393bc6010a0db2b4e2b4b77ccd
Saurabh <devsaurabh086@gmail.com>
2026-02-11 23:38:28 +0530

    Concreate-Class for Both-Product Interfaces

--- src/main/java/com/dev/learning/BurgerFactoryDemo.java
+++ src/main/java/com/dev/learning/BurgerFactoryDemo.java
@@ -9,6 +9,33 @@ interface GarlicBread{
     void createGarlicBread();
 }
 
+//singh-type-product
+class NormalBurger implements Burger{
+    public void createBurger(){
+        System.out.println("This is a normal burger");
+    }
+}
+class SandardBurger implements Burger{
+    public void createBurger(){
+        System.out.println("This is a standard burger");
+    }
+}
+class PremiumBurger implements Burger{
+    public void createBurger(){
+        System.out.println("This is a premium burger");
+    }
+}
+class NormalGarlicBread implements GarlicBread{
+    public void createGarlicBread(){
+        System.out.println("This is a normal garlic bread");
+    }
+}
+class CheeseGarlicBread implements GarlicBread{
+    public void createGarlicBread(){
+        System.out.println("This is a cheese garlic bread");
+    }
+}
+
 
 
 
