2caa250f8bf3710611734c870a2fdd4a7bbde893
Saurabh <devsaurabh086@gmail.com>
2026-02-14 23:15:39 +0530

    Product-Concrete-Implementations

--- src/main/java/com/dev/learning/AbstractNotification.java
+++ src/main/java/com/dev/learning/AbstractNotification.java
@@ -1,3 +1,21 @@
 interface Payment {
     void processPayment(double amount);
 }
+
+class CreditCardPayment implements Payment {
+    public void processPayment(double amount) {
+        System.out.println("Processing ₹" + amount + " via Credit Card");
+    }
+}
+
+class UPIPayment implements Payment {
+    public void processPayment(double amount) {
+        System.out.println("Processing ₹" + amount + " via UPI");
+    }
+}
+
+class PayPalPayment implements Payment {
+    public void processPayment(double amount) {
+        System.out.println("Processing ₹" + amount + " via PayPal");
+    }
+}
