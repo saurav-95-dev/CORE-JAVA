432ac572858d38436c58d1fb4d137bead1d462fc
Saurabh <devsaurabh086@gmail.com>
2026-02-16 23:17:32 +0530

    NotificationFactory--Static

--- src/main/java/com/dev/learning/Notification.java
+++ src/main/java/com/dev/learning/Notification.java
@@ -1,3 +1,41 @@
 interface Notification {
     void send(String message);
 }
+
+class EmailNotification implements Notification {
+    public void send(String message) {
+        System.out.println("Sending EMAIL: " + message);
+    }
+}
+
+class SMSNotification implements Notification {
+    public void send(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+class PushNotification implements Notification {
+    public void send(String message) {
+        System.out.println("Sending PUSH notification: " + message);
+    }
+}
+
+class NotificationFactory {
+
+    public static Notification createNotification(String type) {
+
+        if(type.equalsIgnoreCase("email")) {
+            return new EmailNotification();
+        }
+        else if(type.equalsIgnoreCase("sms")) {
+            return new SMSNotification();
+        }
+        else if(type.equalsIgnoreCase("push")) {
+            return new PushNotification();
+        }
+        else {
+            throw new IllegalArgumentException("Invalid notification type");
+        }
+    }
+}
+
