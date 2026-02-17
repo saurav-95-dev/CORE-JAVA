10273aa14299520ddadceb0adfd6305e024ad761
Saurabh <devsaurabh086@gmail.com>
2026-02-17 23:23:40 +0530

    Concrete Product Implementations

--- src/main/java/com/dev/learning/Notification.java
+++ src/main/java/com/dev/learning/Notification.java
@@ -1,3 +1,21 @@
 interface Vehicle {
     void manufacture();
 }
+
+class Car implements Vehicle {
+    public void manufacture() {
+        System.out.println("Manufacturing a Car");
+    }
+}
+
+class Bike implements Vehicle {
+    public void manufacture() {
+        System.out.println("Manufacturing a Bike");
+    }
+}
+
+class Truck implements Vehicle {
+    public void manufacture() {
+        System.out.println("Manufacturing a Truck");
+    }
+}
