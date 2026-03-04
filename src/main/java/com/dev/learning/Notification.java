38d38c9f31bb858c6a7d52a3593bf90b1dd41e40
Saurabh <devsaurabh086@gmail.com>
2026-03-04 22:28:33 +0530

    Concrete Statergies Classes

--- src/main/java/com/dev/learning/Notification.java
+++ src/main/java/com/dev/learning/Notification.java
@@ -4,3 +4,16 @@
 interface PaymentStrategy {
     void pay(int amount);
 }
+
+// Concrete Strategies
+class CreditCardPayment implements PaymentStrategy {
+    public void pay(int amount) {
+        System.out.println("Paid " + amount + " using Credit Card");
+    }
+}
+
+class UpiPayment implements PaymentStrategy {
+    public void pay(int amount) {
+        System.out.println("Paid " + amount + " using UPI");
+    }
+}
