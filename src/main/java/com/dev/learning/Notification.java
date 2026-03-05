90ab306bfd47e6202902d8a3b5d9ca865ed50c42
Saurabh <devsaurabh086@gmail.com>
2026-03-05 22:23:48 +0530

    Factory Class Created for Credit-Card-System =

--- src/main/java/com/dev/learning/Notification.java
+++ src/main/java/com/dev/learning/Notification.java
@@ -12,3 +12,14 @@ class UPI implements Payment {
         System.out.println("Paid " + amount + " using UPI");
     }
 }
+// Factory Class
+class PaymentFactory {
+    public static Payment getPayment(String type) {
+        if (type.equalsIgnoreCase("CREDIT")) {
+            return new CreditCard();
+        } else if (type.equalsIgnoreCase("UPI")) {
+            return new UPI();
+        }
+        return null;
+    }
+}
