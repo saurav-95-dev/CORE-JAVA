f6370c863ff313f46fc837e9c7f7fb162a8f062c
Saurabh <devsaurabh086@gmail.com>
2026-03-05 22:23:02 +0530

    Concrete Class Created-Credit Card System

--- src/main/java/com/dev/learning/Notification.java
+++ src/main/java/com/dev/learning/Notification.java
@@ -1,3 +1,14 @@
 interface Payment {
     void pay(int amount);
 }
+class CreditCard implements Payment {
+    public void pay(int amount) {
+        System.out.println("Paid " + amount + " using Credit Card");
+    }
+}
+
+class UPI implements Payment {
+    public void pay(int amount) {
+        System.out.println("Paid " + amount + " using UPI");
+    }
+}
