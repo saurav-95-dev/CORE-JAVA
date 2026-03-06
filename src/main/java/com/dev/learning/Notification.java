3da0bc9495e01eafbf46322c9d3d41a543bfa514
Saurabh <devsaurabh086@gmail.com>
2026-03-06 20:23:12 +0530

    Static Builder class

--- src/main/java/com/dev/learning/Notification.java
+++ src/main/java/com/dev/learning/Notification.java
@@ -13,5 +13,31 @@ class Computer{
         System.out.println("RAM: " + RAM);
         System.out.println("Storage: " + storage);
     }
+    // Static Builder Class
+    public static class Builder {
+        private String CPU;
+        private String RAM;
+        private String storage;
+
+        public Builder setCPU(String CPU) {
+            this.CPU = CPU;
+            return this;
+        }
+
+        public Builder setRAM(String RAM) {
+            this.RAM = RAM;
+            return this;
+        }
+
+        public Builder setStorage(String storage) {
+            this.storage = storage;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+}
 
 }
