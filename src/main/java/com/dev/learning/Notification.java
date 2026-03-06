4c9afbbf7c2e348d8669075ac3c089fd33c658c4
Saurabh <devsaurabh086@gmail.com>
2026-03-06 20:21:48 +0530

    Computer Builder--Design-Pattern

--- src/main/java/com/dev/learning/Notification.java
+++ src/main/java/com/dev/learning/Notification.java
@@ -1,32 +1,5 @@
-interface Payment {
-    void pay(int amount);
-}
-class CreditCard implements Payment {
-    public void pay(int amount) {
-        System.out.println("Paid " + amount + " using Credit Card");
-    }
-}
-
-class UPI implements Payment {
-    public void pay(int amount) {
-        System.out.println("Paid " + amount + " using UPI");
-    }
-}
-// Factory Class
-class PaymentFactory {
-    public static Payment getPayment(String type) {
-        if (type.equalsIgnoreCase("CREDIT")) {
-            return new CreditCard();
-        } else if (type.equalsIgnoreCase("UPI")) {
-            return new UPI();
-        }
-        return null;
-    }
-}
-
-public class FactoryPaymentExample {
-    public static void main(String[] args) {
-        Payment payment = PaymentFactory.getPayment("UPI");
-        payment.pay(2000);
-    }
+class Computer{
+    private String CPU;
+    private String RAM;
+    private String storage;
 }
