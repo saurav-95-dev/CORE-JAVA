c6d1b17016b033c0a4f9d437c79139f58d3db8ff
Saurabh <devsaurabh086@gmail.com>
2026-03-07 22:47:02 +0530

    Vehicle Contrete Products

--- src/main/java/com/dev/learning/AbstractNotification.java
+++ src/main/java/com/dev/learning/AbstractNotification.java
@@ -1,3 +1,20 @@
 interface Vehicle {
     void start();
 }
+class Car implements Vehicle {
+    public void start() {
+        System.out.println("Car is starting...");
+    }
+}
+
+class Bike implements Vehicle {
+    public void start() {
+        System.out.println("Bike is starting...");
+    }
+}
+
+class Truck implements Vehicle {
+    public void start() {
+        System.out.println("Truck is starting...");
+    }
+}
