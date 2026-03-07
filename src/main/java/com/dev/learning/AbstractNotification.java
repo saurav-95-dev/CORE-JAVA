178183483d3ba129f132256fc4d836ecb4ff23ac
Saurabh <devsaurabh086@gmail.com>
2026-03-07 22:47:28 +0530

    simple Factory-if-else

--- src/main/java/com/dev/learning/AbstractNotification.java
+++ src/main/java/com/dev/learning/AbstractNotification.java
@@ -18,3 +18,21 @@ class Truck implements Vehicle {
         System.out.println("Truck is starting...");
     }
 }
+class VehicleFactory {
+
+    public static Vehicle createVehicle(String type) {
+
+        if(type.equalsIgnoreCase("car")) {
+            return new Car();
+        }
+        else if(type.equalsIgnoreCase("bike")) {
+            return new Bike();
+        }
+        else if(type.equalsIgnoreCase("truck")) {
+            return new Truck();
+        }
+        else {
+            throw new IllegalArgumentException("Invalid vehicle type");
+        }
+    }
+}
