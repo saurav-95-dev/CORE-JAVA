c40c343eb8c6e2d3607b6f930edd6ce81bb38326
Saurabh <devsaurabh086@gmail.com>
2026-03-08 23:08:12 +0530

    Concrete strategy

--- src/main/java/com/dev/learning/AbstractNotification.java
+++ src/main/java/com/dev/learning/AbstractNotification.java
@@ -1,3 +1,24 @@
 interface PaymentStrategy {
     void pay(double amount);
 }
+
+class CreditCardPayment implements PaymentStrategy {
+
+    public void pay(double amount) {
+        System.out.println("Processing ₹" + amount + " using Credit Card");
+    }
+}
+
+class UPIPayment implements PaymentStrategy {
+
+    public void pay(double amount) {
+        System.out.println("Processing ₹" + amount + " using UPI");
+    }
+}
+
+class PayPalPayment implements PaymentStrategy {
+
+    public void pay(double amount) {
+        System.out.println("Processing ₹" + amount + " using PayPal");
+    }
+}
