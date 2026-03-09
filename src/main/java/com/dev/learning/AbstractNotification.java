986731b4e82e8c2118f0cbff92daa8518503ac6e
Saurabh <devsaurabh086@gmail.com>
2026-03-09 21:54:23 +0530

    Payment Factory

--- src/main/java/com/dev/learning/AbstractNotification.java
+++ src/main/java/com/dev/learning/AbstractNotification.java
@@ -22,22 +22,3 @@ class PayPalPayment implements PaymentStrategy {
         System.out.println("Processing ₹" + amount + " using PayPal");
     }
 }
-class PaymentFactory {
-
-    public static PaymentStrategy createPayment(String type) {
-
-        if(type.equalsIgnoreCase("credit")) {
-            return new CreditCardPayment();
-        }
-        else if(type.equalsIgnoreCase("upi")) {
-            return new UPIPayment();
-        }
-        else if(type.equalsIgnoreCase("paypal")) {
-            return new PayPalPayment();
-        }
-        else {
-            throw new IllegalArgumentException("Invalid payment type");
-        }
-    }
-}
-
