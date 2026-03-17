79fe1b4f8aaea5cf308a4a5df1a3ad30396b37fc
Saurabh <devsaurabh086@gmail.com>
2026-03-17 23:32:28 +0530

    Basics of LLD

---
+++ src/main/java/com/dev/learning/LLD.java
@@ -0,0 +1,5 @@
+//All about LLD (Theory):
+/*
+-> LLD deals with mainly 3 things - Scalability , Maintainability, Code-Reusability
+-> Code-Reusability (plug and play model) -- Application sub-layers should be loosely coupled.
+*/
