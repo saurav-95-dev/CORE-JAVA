06558c58eb1c6cdb2c12193c331d968bc757e9a6
Saurabh <devsaurabh086@gmail.com>
2026-03-17 23:36:05 +0530

    System-Design--Basic Theory

--- src/main/java/com/dev/learning/LLD.java
+++ src/main/java/com/dev/learning/LLD.java
@@ -1,5 +1,6 @@
 //All about LLD (Theory):
 /*
--> LLD deals with mainly 3 things - Scalability , Maintainability, Code-Reusability
+-> LLD(Focus mainly on code-structure) deals with mainly 3 things - Scalability , Maintainability, Code-Reusability
 -> Code-Reusability (plug and play model) -- Application sub-layers should be loosely coupled.
+-> HLD deals with : Focus mainly on System architecture , Tech-stack , which DB to use , Server scaling , Cost Optimization.
 */
