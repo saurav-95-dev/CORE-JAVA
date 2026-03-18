e2b6861381de93137572a80d3e035fc2fc7db628
Saurabh <devsaurabh086@gmail.com>
2026-03-18 23:11:51 +0530

    File-Changed---System-Design

--- src/main/java/com/dev/learning/SystemDesign.java
+++ src/main/java/com/dev/learning/SystemDesign.java
@@ -3,4 +3,8 @@
 -> LLD(Focus mainly on code-structure) deals with mainly 3 things - Scalability , Maintainability, Code-Reusability
 -> Code-Reusability (plug and play model) -- Application sub-layers should be loosely coupled.
 -> HLD deals with : Focus mainly on System architecture , Tech-stack , which DB to use , Server scaling , Cost Optimization.
+--------------------------------------------------------------------------------------------------------------------------------
+
+WHY OOPS OVER PROCEDURAL PROGRAMING :
+
 */
