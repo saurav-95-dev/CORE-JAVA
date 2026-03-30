2502a1a9f3cebebd22ddc5eb57b7a9428c1e2041
Saurabh <devsaurabh086@gmail.com>
2026-03-30 23:53:54 +0530

    LLD-Builder Pattern

---
+++ src/main/java/com/dev/learning/Main.java
@@ -0,0 +1,56 @@
+class Computer {
+    private String CPU;
+    private String RAM;
+    private String storage;
+
+    // Private Constructor
+    private Computer(Builder builder) {
+        this.CPU = builder.CPU;
+        this.RAM = builder.RAM;
+        this.storage = builder.storage;
+    }
+
+    public void showConfig() {
+        System.out.println("CPU: " + CPU);
+        System.out.println("RAM: " + RAM);
+        System.out.println("Storage: " + storage);
+    }
+
+    // Static Builder Class
+    public static class Builder {
+        private String CPU;
+        private String RAM;
+        private String storage;
+
+        public Builder setCPU(String CPU) {
+            this.CPU = CPU;
+            return this;
+        }
+
+        public Builder setRAM(String RAM) {
+            this.RAM = RAM;
+            return this;
+        }
+
+        public Builder setStorage(String storage) {
+            this.storage = storage;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+}
+
+public class BuilderExample {
+    public static void main(String[] args) {
+        Computer computer = new Computer.Builder()
+                .setCPU("Intel i7")
+                .setRAM("16GB")
+                .setStorage("1TB SSD")
+                .build();
+
+        computer.showConfig();
+    }
+}
