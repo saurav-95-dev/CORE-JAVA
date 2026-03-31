ac8ffc3ecde5aa4869728a1b21e23488f68bc589
Saurabh <devsaurabh086@gmail.com>
2026-03-31 22:38:00 +0530

    LLD vs HLD vs DSA--Brief

---
+++ src/main/java/com/dev/learning/SystemDesign.java
@@ -0,0 +1,6 @@
+//SYSTEM-DESIGN : THEORY
+/*
+->Focus area of LLD - Scalability , Maintainability, Re-usability
+->Focus area of HLD - Application's tech stack , which DB to use , Server scaling , Cost Optimisation.
+->DSA - Deals with the algorithmic approach to solve a particular problem in building an application.
+*/
