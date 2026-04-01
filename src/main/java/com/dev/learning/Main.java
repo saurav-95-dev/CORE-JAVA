9f351ca5f3bfb1a1cfb267cc0c91ede14fbc81cd
Saurabh <devsaurabh086@gmail.com>
2026-04-01 23:35:01 +0530

    Basic Method on Strings

--- src/main/java/com/dev/learning/Main.java
+++ src/main/java/com/dev/learning/Main.java
@@ -3,9 +3,10 @@ package com.dev.learning;
 class Main{
     public static void main(String[] args) {
         //System.out.println("Inside main function");
-        String name = new String("Saurabh Dev");
+        String name = "Saurabh Dev";
         System.out.println("Hello" + name);
         System.out.println(name.charAt(1));
+        System.out.println(name.concat(" Kumar"));
 
     }
 }
