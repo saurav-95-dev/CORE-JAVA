af32363df82100baab2a8289d13fc228a5d879e1
Saurabh <devsaurabh086@gmail.com>
2026-04-01 23:49:53 +0530

    Immutability of Strings

--- src/main/java/com/dev/learning/Main.java
+++ src/main/java/com/dev/learning/Main.java
@@ -4,9 +4,15 @@ class Main{
     public static void main(String[] args) {
         //System.out.println("Inside main function");
         String name = "Saurabh Dev";
-        System.out.println("Hello" + name);
-        System.out.println(name.charAt(1));
-        System.out.println(name.concat(" Kumar"));
+        name = name + " appended"; //Here we are basically creating a new object .
+        //name variable will in stack will start referring to the new object created in heap with the updated value.
+        //the old value of name object now gets eligible for garbage collection.
+        System.out.println(name);
+        String s1 = "Hi";
+        String s2 = "Hi";
+        //both s1 and s2 will point to the same address in heap memory under string pool constant.
+        System.out.println(s1 == s2);
+
 
     }
 }
