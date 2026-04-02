07b19a5b03bd281a8df99382796ac0c0e3c46f8e
Saurabh <devsaurabh086@gmail.com>
2026-04-02 22:05:11 +0530

    String Pool Optimization & String Immutability Demonstration

--- src/main/java/com/dev/learning/Main.java
+++ src/main/java/com/dev/learning/Main.java
@@ -2,21 +2,12 @@ package com.dev.learning;
 
 class Main{
     public static void main(String[] args) {
-        //System.out.println("Inside main function");
-        String name = "Saurabh Dev";
-        name = name + " appended"; //Here we are basically creating a new object .
-        //name variable will in stack will start referring to the new object created in heap with the updated value.
-        //the old value of name object now gets eligible for garbage collection.
-        System.out.println(name);
-        String s1 = "Hi";
-        String s2 = "Hi";
-        //both s1 and s2 will point to the same address in heap memory under string pool constant.
+        String s1 = "Saurabh";
+        String s2 = "Saurabh"; //string pool optimization
         System.out.println(s1 == s2);
-        //To Mutate Strings we use StringBuffer and StringBuilder.
-        StringBuffer sb = new StringBuffer("Hi");
-        String result = sb.toString();
-        System.out.println(result);
-
+        s2 = s2 + "dev"; //a new object is created here.
+        System.out.println(s1 == s2);
+        //we can have multiple references pointing at same object in heap , because of immutability property of string.
 
     }
 }
