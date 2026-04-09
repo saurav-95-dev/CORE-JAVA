0e0b598bfa8b27718bca9ee7d6e0fc287652afa0
Saurabh <devsaurabh086@gmail.com>
2026-04-09 23:56:27 +0530

    Introduction to AWS -- AWS Regions-1

--- src/main/java/AWS.java
+++ src/main/java/AWS.java
@@ -1,7 +1,12 @@
 //ALL About AWS : PHASE-1:
 //----------------------------------------------------------------------------
 /*
--> Some Common use-cases - Enterprise IT, Backup & Storage, Big Data Analytics.
+# Some Common use-cases - Enterprise IT, Backup & Storage, Big Data Analytics.
 -> Web hosting, Mobile, Social Apps.
 -> Gaming servers running on cloud.
+
+# AWS Regions:
+->AWS-Regions are basically a clusters of data centers.
+->Most AWS-Services are region-scoped.
+
 */
