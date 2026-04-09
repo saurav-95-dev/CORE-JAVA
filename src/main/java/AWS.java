7143e74566b18cf1780a08ffd4a8b03e30875239
Saurabh <devsaurabh086@gmail.com>
2026-04-09 23:48:03 +0530

    Introduction to AWS -- common use-cases

--- src/main/java/AWS.java
+++ src/main/java/AWS.java
@@ -1,2 +1,7 @@
 //ALL About AWS : PHASE-1:
 //----------------------------------------------------------------------------
+/*
+-> Some Common use-cases - Enterprise IT, Backup & Storage, Big Data Analytics.
+-> Web hosting, Mobile, Social Apps.
+-> Gaming servers running on cloud.
+*/
--- src/main/java/com/dev/learning/Main.java
+++ src/main/java/com/dev/learning/Main.java
@@ -2,12 +2,6 @@ package com.dev.learning;
 
 class Main{
     public static void main(String[] args) {
-        String s1 = "Saurabh";
-        String s2 = "Saurabh"; //string pool optimization
-        System.out.println(s1 == s2);
-        s2 = s2 + "dev"; //a new object is created here.
-        System.out.println(s1 == s2);
-        //we can have multiple references pointing at same object in heap , because of immutability property of string.
 
     }
 }
