c0b777d0afd2b0e6c48e104350dc138cf2522a07
Saurabh <devsaurabh086@gmail.com>
2026-04-19 18:09:20 +0530

    AWS Global Infrastructure Basic Overview

--- src/main/java/AWS.java
+++ src/main/java/AWS.java
@@ -15,18 +15,33 @@
 # Factors affecting choice of selecting regions :
 ->Compliance with data governance and legal requirements
 ->Proximity to customers -- Reduced Latency.
-->Available Service within a region
+->Available Service within a .
 ->Pricing
 
 # AWS Availability Zones AZ:
 ->Each region has usually 3 availability zones - min-3 , max-6.
 ->Each AZ has one or more data centers with redundant power , networking and connectivity.
-->They are separate from each other to facilitate disaster recovery.
+->These AZ are separate from each other to facilitate disaster recovery.
 ->They are connected with high bandwidth , ultra low latency networking.
 
 #AWS Point of presence (Edge Locations):
-->AWS has 400+ Points of presence
+->AWS has 400+ Points of presence in 90+ cities in across 40+ countries.
 ->Main Essence - Content is delivered to users with lower latency.
-----------------------------------------------------------------------------------------
+
+#QUICK AWS CONSOLE TOUR:
+->AWS has global service : IAM(identity and access management),
+Route 53(DNS Service),
+CloudFront(Content Delivery Network),
+WAF(Web Application Firewall).
+->Most AWS Service are Region-scoped:
+Amazon EC2(Infrastructure as a service),
+Elastic Beanstalk(Platform as a service),
+Lambda(Function as a  Service),
+Rekognition(Software as a service).
+---------------------------------------------------------------------------------------------------
+### IAM AND AWS CLI :
+
+
+ */
 
 
