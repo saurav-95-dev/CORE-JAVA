3de941d19a342f6c44eb53d88ebf644dfc5e1bbf
Saurabh <devsaurabh086@gmail.com>
2026-05-20 00:24:56 +0530

    JAVA--Static keyword

--- src/main/java/com/dev/learning/Main.java
+++ src/main/java/com/dev/learning/Main.java
@@ -1,28 +1,36 @@
 package com.dev.learning;
 
+class Mobile{
+    String company;
+    int price;
+    static String device;
+
+    static{
+        device = "General";
+        System.out.println("inside static block");
+    }
+    //constructor:
+    public Mobile(){
+        company = "default";
+        price = 0;
+        System.out.println("inside constructor");
+    }
+    static void show(Mobile obj2){
+        System.out.println("inside show");
+        System.out.println("Company:"+obj2.company + ", Price:"+obj2.price + ", Device:"+device);
+    }
+
+}
 class Main{
     public static void main(String[] args) {
-        String name = new String("material"); //name is a reference variable pointing to the string object.
-        //name is a reference variable made in stack which is pointing to string object made in heap memory.
-        System.out.println("Hello " + name);
-        System.out.println(name.charAt(1));
-        //Immutability of Strings :
-        String firstName = "Saurabh"; //this will be eligible for garbage collection
-        firstName = firstName + " Dev"; //here we are basically creating a new object
-        System.out.println(firstName);
-
-        String s1 = "common";
-        String s2 = "common";
-        System.out.println(s1==s2);
-        //There is a special section in heap memory called string constant pool.
-        //How immutability of string help in string pool optimization.
-        String str1 = "Person1";
-        String str2 = "Person1";
-
-        str2 = str2 + " modified";
-        System.out.println(str1); //Person1
-        System.out.println(str2); //Person modified
-        //str1 will be unaffected .
+        Mobile obj1 = new Mobile();
+        obj1.company = "Apple";
+        obj1.price = 100;
+        Mobile obj2 = new Mobile();
+        obj2.company = "Nokia";
+        obj2.price = 100000;
+        Mobile.show(obj2);
+        //obj2.show();
 
 
 
