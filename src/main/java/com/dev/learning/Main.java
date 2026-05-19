f14787bdbbd042da31d55cee3283fa5a05296928
Saurabh <devsaurabh086@gmail.com>
2026-05-19 21:35:27 +0530

    Basics of Strings

--- src/main/java/com/dev/learning/Main.java
+++ src/main/java/com/dev/learning/Main.java
@@ -2,6 +2,10 @@ package com.dev.learning;
 
 class Main{
     public static void main(String[] args) {
+        String name = new String("material"); //name is a reference variable pointing to the string object.
+        //name is a reference variable made in stack which is pointing to string object made in heap memory.
+        System.out.println("Hello " + name);
+        System.out.println(name.charAt(1));
 
     }
 }
