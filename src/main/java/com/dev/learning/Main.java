d4914dfe49eeafb1fc08647d679c007743fe74c5
Saurabh <devsaurabh086@gmail.com>
2026-05-21 23:14:13 +0530

    Basics of Static Keyword

--- src/main/java/com/dev/learning/Main.java
+++ src/main/java/com/dev/learning/Main.java
@@ -1,17 +1,34 @@
 package com.dev.learning;
 
-//String concept revision :
+class Computer{
+    int price;
+    String company;
+    static String  deviceType; //member of class
+    //constructor:
+    public Computer(){
+        price = 0;
+        company = "Ultikhopdi";
+        deviceType = "not decided";
+    }
+    public static void show(Computer obj1){
+        System.out.println("Price:" + obj1.price + " company:" + obj1.company + " deviceType:" + deviceType);
+    }
+
+}
+
 class Main{
     public static void main(String[] args){
-        //How String Immutability helps in string pool optimization:
-        String s1 = "Saurabh";
-        String s2 = "saurabh";
-        String s3 = "Saurabh";
-        System.out.println(s1.equals(s3));
-        //in above case only one object is created and both s1 and s3 points to teh same object in the heap memory.
-        s3 = s3 + " Dev"; //here we have created a separate new object in heap and s3 is now pointing to that new location.
+         //Concept of static keyword :
+         Computer obj1 = new Computer();
+         obj1.price = 100;
+         obj1.company = "HP";
+         obj1.deviceType = "Laptop";
+         Computer obj2 = new Computer();
+         obj2.price = 200;
+         obj2.company = "Apple";
+         Computer.deviceType = "Laptop";
+         obj1.show(obj1);
 
-        System.out.println(s1==s3);
-        System.out.println(s3);
+
     }
 }
