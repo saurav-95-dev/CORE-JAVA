a44ad6b1c7280648cf553776f0ef7793ae8817ac
Saurabh <devsaurabh086@gmail.com>
2026-05-25 23:27:47 +0530

    Load and Register the Driver

--- src/main/java/com/dev/learning/JDBC.java
+++ src/main/java/com/dev/learning/JDBC.java
@@ -1,7 +1,8 @@
 package  com.dev.learning;
 import java.sql.*;
 public class JDBC {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
     }
 }
