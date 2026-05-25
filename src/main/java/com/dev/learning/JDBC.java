daa95a7819b9f376ff6a07b7768eeee2a59de0d0
Saurabh <devsaurabh086@gmail.com>
2026-05-25 23:47:49 +0530

    Testing for DB Connection

--- src/main/java/com/dev/learning/JDBC.java
+++ src/main/java/com/dev/learning/JDBC.java
@@ -1,15 +1,30 @@
-package  com.dev.learning;
+package com.dev.learning;
 import java.sql.*;
-public class JDBC {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
+
+class DemoClass {
+    public static void main(String[] args) throws Exception {
 
         String url = "jdbc:mysql://localhost:3306/org";
         String username = "root";
-        String password = "Saurabh@123"; // your real password
+        String password = "xyz"; // your real password
         String query = "select * from worker";
 
-        Connection con = DriverManager.getConnection(url, username, password);
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
+        Connection con = DriverManager.getConnection(url, username, password);
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("select * from worker");
+
+        while (rs.next()) {
+            System.out.println(
+                    rs.getInt("id") + " " +
+                            rs.getString("sname") + " " +
+                            rs.getString("sage")
+            );
+        }
+
+        rs.close();
+        st.close();
+        con.close();
     }
 }
