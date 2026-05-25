c82341403c3e998ef685f7dcdcb4427d2d518e75
Saurabh <devsaurabh086@gmail.com>
2026-05-25 23:40:35 +0530

    Eastablishing Connection with the DB

--- src/main/java/com/dev/learning/JDBC.java
+++ src/main/java/com/dev/learning/JDBC.java
@@ -1,8 +1,15 @@
 package  com.dev.learning;
 import java.sql.*;
 public class JDBC {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
+
+        String url = "jdbc:mysql://localhost:3306/org";
+        String username = "root";
+        String password = "Saurabh@123"; // your real password
+        String query = "select * from worker";
+
+        Connection con = DriverManager.getConnection(url, username, password);
 
     }
 }
