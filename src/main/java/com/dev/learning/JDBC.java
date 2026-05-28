ab911accb51d916ed1d1fe878c5fb7f3ede61b1c
Saurabh <devsaurabh086@gmail.com>
2026-05-28 23:18:11 +0530

    Exception Handling in JDBC and importance of finally block

--- src/main/java/com/dev/learning/JDBC.java
+++ src/main/java/com/dev/learning/JDBC.java
@@ -2,35 +2,53 @@ package  com.dev.learning;
 import java.sql.*;
 public class JDBC {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        //1)Load and Register the driver:
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        //2)Establishing the connection:
-        String url = "jdbc:mysql://localhost:3306/jdbclearning";
-        String username = "root";
-        String password = "Saurabh@123"; // your real password
-        Connection con = DriverManager.getConnection(url, username, password);
-        //Create statement :
-        Statement st = con.createStatement();
-        //execute logic :
-        //common method for CRUD in JDBC :
-        String query = "insert into Student values (2 , 'Laukik' , 32 , 'Gurugram')";
-        boolean status = st.execute(query);
-        if(status){
-            ResultSet rs = st.getResultSet();
-            while(rs.next()){
-                System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3)+" "+rs.getString(4));
+        Connection con = null;
+        Statement st = null;
+        //Handling exception:
+        try{
+            //1)Load and Register the driver:
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //2)Establishing the connection:
+            String url = "jdbc:mysql://localhost:3306/jdbclearning";
+            String username = "root";
+            String password = "Saurabh@123"; // your real password
+            con = DriverManager.getConnection(url, username, password);
+            //Create statement :
+            st = con.createStatement();
+            //execute logic :
+            //common method for CRUD in JDBC :
+            String query = "insert into Student values (3 , 'Mukund' , 44 , 'Noida')";
+            boolean status = st.execute(query);
+            if(status){
+                ResultSet rs = st.getResultSet();
+                while(rs.next()){
+                    System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3)+" "+rs.getString(4));
 
+                }
             }
-        }
-        else{
-            int rows = st.getUpdateCount();
-            if (rows == 0) {
-                System.out.println("Operation failed");
-            }else{
-                System.out.println("Operation successful");
+            else{
+                int rows = st.getUpdateCount();
+                if (rows == 0) {
+                    System.out.println("Operation failed");
+                }else{
+                    System.out.println("Operation successful");
+                }
             }
+
         }
-        st.close();
-        con.close();
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        finally{
+            //resource closing :
+            st.close();
+            con.close();
+        }
     }
 }
