6064970a5405deee4c61cb3fd8d971b63fda1163
Saurabh <devsaurabh086@gmail.com>
2026-06-06 22:09:56 +0530

    INSERT Operation using PreparedStatement in JDBC

--- src/main/java/com/dev/learning/JDBCPreparedStatement.java
+++ src/main/java/com/dev/learning/JDBCPreparedStatement.java
@@ -1,71 +1,52 @@
 package com.dev.learning;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Scanner;
+import java.sql.*;
 
+//PERFORMING CRUD OPERATION IN JDBC VIA UTILITY CLASS VIA PREPARED-STATEMENT:
 class JDBCPreparedStatement {
-    public static void main(String[] args)  {
-          Connection con = null;
-          PreparedStatement ps = null;
-          try{
-              //Step-1 : Create connection:
-              con = UtilityJDBCPreparedStatement.getConnection();
-              ps = con.prepareStatement("insert into student (id , sname ,sage ,scity) values(? ,? ,? ,?)");
-              Scanner sc = new Scanner(System.in);
-              System.out.print("Enter ID: ");
-              int id = sc.nextInt();
-              sc.nextLine();
+    public static void main(String[] args) {
+        //INSERTION OPERATION (non-select operation) VIA PREPARED STATEMENT:
+        Connection con = null;
+        PreparedStatement ps = null;
+        try{
+            //Step:1 Load and register the driver and get the connection:
+            con = UtilityJDBCPreparedStatement.getConnection();
+            ps = con.prepareStatement("insert into Student values (10 , 'Pandu' , 11 , 'Banglore')"); //Directly pass query here
+            boolean status = ps.execute();
+            if(status){
+                //means it's data retrieval
+                ResultSet rs = ps.getResultSet();
+                while(rs.next()){
+                    System.out.println(rs.getInt(1));
+                    System.out.println(rs.getString(2));
+                    System.out.println(rs.getInt(3));
+                    System.out.println(rs.getString(4));
+                }
 
-              System.out.print("Enter Name: ");
-              String sname = sc.nextLine();
+            }
+            else{
+                //it's data manipulation:
+                int rows = ps.getUpdateCount();
+                if(rows==0){
+                    System.out.println("No rows affected");
+                }
+                else{
+                    System.out.println("Rows affected: "+rows + " Operation Successful");
+                }
+            }
 
-              System.out.print("Enter Age: ");
-              int sage = sc.nextInt();
-              sc.nextLine();
-
-              System.out.print("Enter City: ");
-              String scity = sc.nextLine();
-              ps.setInt(1, id);
-              ps.setString(2, sname);
-              ps.setInt(3, sage);
-              ps.setString(4, scity);
-              boolean status = ps.execute();
-              if(status){
-                  ResultSet rs = ps.getResultSet();
-                  while(rs.next()){
-                      System.out.println(rs.getInt(1) + "," + rs.getString(2) + "," + rs.getInt(3));
-
-                  }
-              }
-              else{
-                  int rows = ps.getUpdateCount();
-                  System.out.println(rows + " rows updated");
-                  if(rows==0){
-                      System.out.println("No rows updated");
-                  }
-                  else{
-                      System.out.println("rows updated successfully" );
-                  }
-
-              }
-
-          } catch (SQLException e) {
-              throw new RuntimeException(e);
-          }
-          catch(Exception e){
-              e.printStackTrace();
-          }
-          finally{
-             try{
-                 UtilityJDBCPreparedStatement.closeConnection(ps , con);
-             }
-             catch(Exception e){
-                 e.printStackTrace();
-             }
-          }
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        finally{
+            try{
+                UtilityJDBCPreparedStatement.closeConnection(con, ps);
+            }
+            catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
 
 
     }
--- src/main/java/com/dev/learning/UtilityJDBCPreparedStatement.java
+++ src/main/java/com/dev/learning/UtilityJDBCPreparedStatement.java
@@ -1,30 +1,26 @@
 package com.dev.learning;
-//In this uility class we will do 2 things -> Create connection and close the resource.
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 class UtilityJDBCPreparedStatement {
-     static{
-         //Step1:load and register the driver:
-         try {
-             Class.forName("com.mysql.cj.jdbc.Driver");
-         } catch (ClassNotFoundException e) {
-             throw new RuntimeException(e);
-         }
-     }
-     //now define getConnection method :
-    public static Connection getConnection() throws SQLException {
-           String url = "jdbc:mysql://localhost:3306/jdbclearning";
-           String username = "root";
-           String password = "Saurabh@123";
-           return DriverManager.getConnection(url, username, password);
+    static{
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
     }
-    public static void closeConnection(Statement ps, Connection con) throws SQLException {
-         ps.close();
-         con.close();
+    //support methods:
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/jdbclearning";
+        String username = "root";
+        String password = "xyz"; // your real password
+        return DriverManager.getConnection(url, username, password);
+    }
+    public static void closeConnection(Connection con , PreparedStatement ps) throws SQLException {
+        ps.close();
+        con.close();
+
     }
 }
